b1fe449deb4823db8239d6c5407fa75dc81daa37
Rudi Grinberg <me@rgrinberg.com>
2019-12-18 14:26:56 +0700

    Add e2e tests for foldingRange
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ ocaml-lsp-server/test/e2e/__tests__/textDocument-foldingRange.test.ts
@@ -0,0 +1,76 @@
+import outdent from "outdent";
+import * as LanguageServer from "../src/LanguageServer";
+
+import * as Protocol from "vscode-languageserver-protocol";
+import * as Types from "vscode-languageserver-types";
+
+describe("textDocument/foldingRange", () => {
+    let languageServer = null;
+
+    beforeEach(async () => {
+        languageServer = await LanguageServer.startAndInitialize();
+    });
+
+    afterEach(async () => {
+        await LanguageServer.exit(languageServer);
+        languageServer = null;
+    });
+
+    async function openDocument(source) {
+        await languageServer.sendNotification("textDocument/didOpen", {
+            textDocument: Types.TextDocumentItem.create(
+                "file:///test.ml",
+                "txt",
+                0,
+                source
+            )
+        });
+    }
+
+    async function foldingRange() {
+        return await languageServer.sendRequest("textDocument/foldingRange", {
+            textDocument: Types.TextDocumentIdentifier.create("file:///test.ml")
+        });
+    }
+
+    it("returns folding ranges for modules", async () => {
+        await openDocument(outdent`
+          module type X = sig
+            type t
+          end
+
+          module X = struct
+            module Y = struct
+              let bar = 42
+            end
+            let foo = 3
+          end
+        `);
+
+        let result = await foldingRange();
+        expect(result).toMatchObject([
+            {
+                "endCharacter": null,
+                "endLine": 2,
+                "kind": "region",
+                "startCharacter": null,
+                "startLine": 0,
+            },
+            {
+                "endCharacter": null,
+                "endLine": 7,
+                "kind": "region",
+                "startCharacter": null,
+                "startLine": 5,
+            },
+            {
+                "endCharacter": null,
+                "endLine": 9,
+                "kind": "region",
+                "startCharacter": null,
+                "startLine": 4,
+            },
+        ]);
+    });
+
+})
