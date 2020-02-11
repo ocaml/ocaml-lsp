83eeef8156b5ec2d5328d9ed1173c62dea142060
Rudi Grinberg <me@rgrinberg.com>
2020-02-11 19:18:32 +0000

    Add name resolution pass
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/bin/typescript/ts_types.ml
+++ lsp/bin/typescript/ts_types.ml
@@ -1,3 +1,5 @@
+open Import
+
 module Named = struct
   type 'a t =
     { name : string
@@ -22,8 +24,14 @@ module type S = sig
   type ident
 
   type field_def =
-    | Single of { optional : bool ; typ: typ }
-    | Pattern of  { pat : typ; typ: typ }
+    | Single of
+        { optional : bool
+        ; typ : typ
+        }
+    | Pattern of
+        { pat : typ
+        ; typ : typ
+        }
 
   and field = field_def Named.t
 
@@ -51,10 +59,19 @@ module type S = sig
   and t = decl Named.t
 end
 
-module Make (Ident : sig type t end) = struct
+module Make (Ident : sig
+  type t
+end) =
+struct
   type field_def =
-    | Single of { optional : bool ; typ: typ }
-    | Pattern of  { pat : typ; typ: typ }
+    | Single of
+        { optional : bool
+        ; typ : typ
+        }
+    | Pattern of
+        { pat : typ
+        ; typ : typ
+        }
 
   and field = field_def Named.t
 
@@ -83,7 +100,7 @@ module Make (Ident : sig type t end) = struct
 end
 
 module Unresolved = struct
-  include Make(String)
+  include Make (String)
 
   let enum ~name ~constrs : Enum.t Named.t = { Named.name; data = constrs }
 
@@ -94,7 +111,7 @@ module Unresolved = struct
     { Named.name; data = Pattern { pat; typ } }
 
   let named_field ?(optional = false) typ name =
-    { Named.name; data = Single { optional ; typ } }
+    { Named.name; data = Single { optional; typ } }
 end
 
 module type Prim_intf = sig
@@ -107,13 +124,16 @@ module type Prim_intf = sig
     | Number
     | Any
     | Object
+    | List
     | Resolved of resolved
 
   val of_string : string -> resolve:(string -> t) -> t
 end
 
-module Prim_make (Resolved : sig type t end) = struct
-
+module Prim_make (Resolved : sig
+  type t
+end) =
+struct
   type t =
     | Null
     | String
@@ -121,6 +141,7 @@ module Prim_make (Resolved : sig type t end) = struct
     | Number
     | Any
     | Object
+    | List
     | Resolved of Resolved.t
 
   let of_string s ~resolve =
@@ -130,9 +151,55 @@ module Prim_make (Resolved : sig type t end) = struct
     | "boolean" -> Bool
     | "number" -> Number
     | "any" -> Any
+    | "array" -> List
     | "object" -> Object
     | _ -> resolve s
 end
 
-module rec Resolved : S with type ident := Prim.t = Make(Prim)
-and Prim : Prim_intf with type resolved := Resolved.t = Prim_make(Resolved)
+module rec Resolved : (S with type ident := Prim.t) = Make (Prim)
+and Prim : (Prim_intf with type resolved := Resolved.t) = Prim_make (Resolved)
+
+let rec resolve_all ts ~(names : string -> Unresolved.t) : Resolved.t list =
+  List.map ts ~f:(resolve ~names)
+
+and resolve (t : Unresolved.t) ~names : Resolved.t =
+  let data : Resolved.decl =
+    match t.data with
+    | Interface i -> Interface (resolve_interface i ~names)
+    | Alias t -> Type (resolve_type t ~names)
+    | Type t -> Type (resolve_type t ~names)
+    | Enum_anon a -> Enum_anon a
+  in
+  { t with Named.data }
+
+and resolve_ident i ~names =
+  Prim.of_string i ~resolve:(fun s -> Resolved (resolve (names s) ~names))
+
+and resolve_type t ~names : Resolved.typ =
+  match t with
+  | Literal l -> Literal l
+  | Ident i -> Ident (resolve_ident ~names i)
+  | Sum l -> Sum (List.map ~f:(resolve_type ~names) l)
+  | Tuple l -> Tuple (List.map ~f:(resolve_type ~names) l)
+  | App (f, x) -> App (resolve_type ~names f, resolve_type ~names x)
+  | List t -> List (resolve_type t ~names)
+  | Record fields -> Record (List.map ~f:(resolve_field ~names) fields)
+
+and resolve_interface i ~names : Resolved.interface =
+  { extends = List.map ~f:(resolve_ident ~names) i.extends
+  ; params = i.params
+  ; fields = List.map ~f:(resolve_field ~names) i.fields
+  }
+
+and resolve_field f ~names : Resolved.field =
+  let data : Resolved.field_def =
+    match f.data with
+    | Single { optional; typ } ->
+      let typ = resolve_type ~names typ in
+      Single { optional; typ }
+    | Pattern { pat; typ } ->
+      let typ = resolve_type ~names typ in
+      let pat = resolve_type ~names pat in
+      Pattern { pat; typ }
+  in
+  { f with Named.data }
--- lsp/bin/typescript/typescript.ml
+++ lsp/bin/typescript/typescript.ml
@@ -8,34 +8,31 @@ type test =
   }
 
 let name_table (defns : Unresolved.t list) =
-  List.map defns ~f:(fun (def : _ Named.t) ->
-      def.name, def.data)
+  List.map defns ~f:(fun (def : _ Named.t) -> (def.name, def))
   |> String.Map.of_list_reducei ~f:(fun name v1 v2 ->
-      let open Unresolved in
-      match v1, v2 with
-      | Enum_anon _, _ -> v1
-      | _, Enum_anon _ -> v2
-      | _, _ ->
-        if v1 = v2 then
-          v1
-        else
-          let open Dyn.Encoder in
-          Code_error.raise "definition conflict"
-            [ "name", string name
-            ]
-    )
+         let open Unresolved in
+         match (v1.Named.data, v2.data) with
+         | Enum_anon _, _ -> v1
+         | _, Enum_anon _ -> v2
+         | _, _ ->
+           if v1 = v2 then
+             v1
+           else
+             let open Dyn.Encoder in
+             Code_error.raise "definition conflict" [ ("name", string name) ])
 
 let test_snippets s =
   let fails, succs =
     List.partition_map s ~f:(fun s ->
         let lexbuf = Lexing.from_string s in
-        try
-          Right (Ts_parser.main Ts_lexer.token lexbuf)
+        try Right (Ts_parser.main Ts_lexer.token lexbuf)
         with exn -> Left { snippet = s; loc = lexbuf.lex_curr_p; exn })
   in
-  ignore (name_table (List.concat succs));
+  let succs = List.concat succs in
+  let name_table = name_table succs in
+  let names x = String.Map.find_exn name_table x in
+  ignore (Ts_types.resolve_all succs ~names);
   fails
-
 
 let pp_results ppf tests =
   List.iteri tests ~f:(fun i { snippet; loc; exn } ->
