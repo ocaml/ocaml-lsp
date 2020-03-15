ace566dceb66e02200c624bdb1423be951bdf78c
Rudi Grinberg <me@rgrinberg.com>
2020-03-15 10:06:38 +0100

    split completion into multiple functions
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/compl.ml
+++ ocaml-lsp-server/src/compl.ml
@@ -12,84 +12,72 @@ let completion_kind kind : CompletionItemKind.t option =
   | `Type -> Some TypeParameter
   | `MethodCall -> Some Method
 
+let make_string chars =
+  let chars = Array.of_list chars in
+  String.init (Array.length chars) ~f:(Array.get chars)
+
+let prefix_of_position source position =
+  match Msource.text source with
+  | "" -> ""
+  | text ->
+    let len = String.length text in
+
+    let rec find prefix i =
+      if i < 0 then
+        make_string prefix
+      else if i >= len then
+        find prefix (i - 1)
+      else
+        let ch = text.[i] in
+        (* The characters for an infix function are missing *)
+        match ch with
+        | 'a' .. 'z'
+        | 'A' .. 'Z'
+        | '0' .. '9'
+        | '.'
+        | '\''
+        | '_' ->
+          find (ch :: prefix) (i - 1)
+        | _ -> make_string prefix
+    in
+
+    let (`Offset index) = Msource.get_offset source position in
+    find [] (index - 1)
+
+let range_prefix (lsp_position : Position.t) prefix : Range.t =
+  let start =
+    let len = String.length prefix in
+    let character = lsp_position.character - len in
+    { lsp_position with character }
+  in
+  { Range.start; end_ = lsp_position }
+
+let item index entry =
+  let prefix, (entry : Query_protocol.Compl.entry) =
+    match entry with
+    | `Keep entry -> (`Keep, entry)
+    | `Replace (range, entry) -> (`Replace range, entry)
+  in
+  let kind = completion_kind entry.kind in
+  let textEdit =
+    match prefix with
+    | `Keep -> None
+    | `Replace range -> Some { TextEdit.range; newText = entry.name }
+  in
+  CompletionItem.create ~label:entry.name ?kind ~detail:entry.desc
+    ~deprecated:
+      entry.deprecated
+      (* Without this field the client is not forced to respect the order
+         provided by merlin. *)
+    ~sortText:(Printf.sprintf "%04d" index)
+    ?textEdit ()
+
+let completion_kinds =
+  [ `Constructor; `Labels; `Modules; `Modules_type; `Types; `Values; `Variants ]
+
 let complete doc position =
   let lsp_position = position in
   let position = Position.logical position in
-
-  let make_string chars =
-    let chars = Array.of_list chars in
-    String.init (Array.length chars) ~f:(Array.get chars)
-  in
-
-  let prefix_of_position source position =
-    match Msource.text source with
-    | "" -> ""
-    | text ->
-      let len = String.length text in
-
-      let rec find prefix i =
-        if i < 0 then
-          make_string prefix
-        else if i >= len then
-          find prefix (i - 1)
-        else
-          let ch = text.[i] in
-          (* The characters for an infix function are missing *)
-          match ch with
-          | 'a' .. 'z'
-          | 'A' .. 'Z'
-          | '0' .. '9'
-          | '.'
-          | '\''
-          | '_' ->
-            find (ch :: prefix) (i - 1)
-          | _ -> make_string prefix
-      in
-
-      let (`Offset index) = Msource.get_offset source position in
-      find [] (index - 1)
-  in
-
-  let range_prefix prefix : Range.t =
-    let start =
-      let len = String.length prefix in
-      let character = lsp_position.character - len in
-      { lsp_position with character }
-    in
-    { Range.start; end_ = lsp_position }
-  in
-
-  let item index entry =
-    let prefix, (entry : Query_protocol.Compl.entry) =
-      match entry with
-      | `Keep entry -> (`Keep, entry)
-      | `Replace (range, entry) -> (`Replace range, entry)
-    in
-    let kind = completion_kind entry.kind in
-    let textEdit =
-      match prefix with
-      | `Keep -> None
-      | `Replace range -> Some { TextEdit.range; newText = entry.name }
-    in
-    CompletionItem.create ~label:entry.name ?kind ~detail:entry.desc
-      ~deprecated:
-        entry.deprecated
-        (* Without this field the client is not forced to respect the order
-           provided by merlin. *)
-      ~sortText:(Printf.sprintf "%04d" index)
-      ?textEdit ()
-  in
-
-  let completion_kinds =
-    [ `Constructor
-    ; `Labels
-    ; `Modules
-    ; `Modules_type
-    ; `Types
-    ; `Values
-    ; `Variants
-    ]
-  in
 
   let prefix = prefix_of_position (Document.source doc) position in
   log ~title:Logger.Title.Debug "completion prefix: |%s|" prefix;
@@ -127,7 +115,7 @@ let complete doc position =
       let { Query_protocol.Compl.entries; context = _ } =
         Query_commands.dispatch pipeline expand
       in
-      let range = range_prefix prefix in
+      let range = range_prefix lsp_position prefix in
       List.map ~f:(fun entry -> `Replace (range, entry)) entries
   in
   let items = List.mapi ~f:item items in
