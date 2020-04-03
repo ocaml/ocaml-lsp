13c6e303365c518e7bc24cdcdd1f33f1ad8e0355
cannorin <cannorin@users.noreply.github.com>
2020-04-03 19:04:19 +0900

    Refactor Lsp.Snippet

--- lsp/src/snippet.ml
+++ lsp/src/snippet.ml
@@ -1,74 +1,157 @@
 open! Import
 
-module Grammar = struct
-  type var =
-    | TM_SELECTED_TEXT
-    | TM_CURRENT_LINE
-    | TM_CURRENT_WORD
-    | TM_LINE_INDEX
-    | TM_LINE_NUMBER
-    | TM_FILENAME
-    | TM_FILENAME_BASE
-    | TM_DIRECTORY
-    | TM_FILEPATH
+type var =
+  | TM_SELECTED_TEXT
+  | TM_CURRENT_LINE
+  | TM_CURRENT_WORD
+  | TM_LINE_INDEX
+  | TM_LINE_NUMBER
+  | TM_FILENAME
+  | TM_FILENAME_BASE
+  | TM_DIRECTORY
+  | TM_FILEPATH
 
-  type variable_transform = {
-    regex: string;
-    format_string: string;
-    regex_options: string option
+type variable_transform =
+  { regex : string
+  ; format_string : string
+  ; regex_options : string option
   }
 
-  type t =
-    | Tabstop of int * [ `Placeholder of t | `Choice of string list | `None ]
-    | Variable of var * [ `Placeholder of t | `Transform of variable_transform | `None ]
-    | Text of string
+type t =
+  | Tabstop of
+      int option * [ `Placeholder of t | `Choice of string list | `None ]
+  | Variable of
+      var * [ `Placeholder of t | `Transform of variable_transform | `None ]
+  | Text of string
+  | Concat of t * t
 
-  let tabstop index = Tabstop (index, `None)
-  let placeholder index content = Tabstop (index, `Placeholder content)
-  let choice index values =
-    if List.is_empty values then raise (Invalid_argument "choice must have non empty values")
-    else Tabstop (index, `Choice values)
-  let variable ?(opt=`None) var = Variable (var, opt)
+module Grammar = struct
+  let tabstop index = Tabstop (Some index, `None)
+
+  let placeholder ?index content = Tabstop (index, `Placeholder content)
+
+  let choice ?index values =
+    if List.is_empty values then
+      raise (Invalid_argument "choice must have non empty values")
+    else
+      Tabstop (index, `Choice values)
+
+  let variable ?(opt = `None) var = Variable (var, opt)
+
   let variable_transform ~regex ?regex_options ~format_string =
     { regex; regex_options; format_string }
+
   let text str = Text str
+
+  let ( ^^ ) lhs rhs = Concat (lhs, rhs)
+
+  let ( @+ ) lhs_str rhs = Concat (Text lhs_str, rhs)
+
+  let ( +@ ) lhs rhs_str = Concat (lhs, Text rhs_str)
 end
 
-type t = Grammar.t
+open Grammar
 
-let escape ?(in_choice=false) str =
-  let str =
-    str |> String.replace_all ~pattern:"$" ~with_:"\\$"
-        |> String.replace_all ~pattern:"}" ~with_:"\\}"
-        |> String.replace_all ~pattern:"\\" ~with_:"\\\\"
+let concat ?sep ts =
+  let rec go = function
+    | [] -> text ""
+    | [ x ] -> x
+    | x :: rest -> (
+      match sep with
+      | None -> Concat (x, go rest)
+      | Some s -> Concat (x, Concat (s, go rest)) )
   in
-  if not in_choice then str
-  else
-    str |> String.replace_all ~pattern:"," ~with_:"\\,"
-        |> String.replace_all ~pattern:"|" ~with_:"\\|"
+  go ts
 
-let rec to_string (snippet: t) : string =
-  let open Printf in
-  match snippet with
-  | Text s -> escape s
-  | Tabstop (i, `None) -> sprintf "$%d" i
-  | Tabstop (i, `Placeholder s) -> sprintf "${%d:%s}" i (to_string s)
-  | Tabstop (i, `Choice values) ->
-    sprintf "${%d|%s|}" i (values |> List.map ~f:(escape ~in_choice:true) |> String.concat ~sep:",")
-  | Variable (var, opt) ->
-    let var =
-      match var with
-      | TM_SELECTED_TEXT -> "TM_SELECTED_TEXT"
-      | TM_CURRENT_LINE -> "TM_CURRENT_LINE"
-      | TM_CURRENT_WORD -> "TM_CURRENT_WORD"
-      | TM_LINE_INDEX -> "TM_LINE_INDEX"
-      | TM_LINE_NUMBER -> "TM_LINE_NUMBER"
-      | TM_FILENAME -> "TM_FILENAME"
-      | TM_FILENAME_BASE -> "TM_FILENAME_BASE"
-      | TM_DIRECTORY -> "TM_DIRECTORY"
-      | TM_FILEPATH -> "TM_FILEPATH"
-    in
-    match opt with
-    | `None -> sprintf "$%s" var
-    | `Placeholder s -> sprintf "${%s:%s}" var (to_string s)
-    | `Transform t -> sprintf "${%s/%s/%s/%s}" var t.regex t.format_string (Option.value ~default:"" t.regex_options)
+let escape ?(in_choice = false) str =
+  let str =
+    str
+    |> String.replace_all ~pattern:"$" ~with_:"\\$"
+    |> String.replace_all ~pattern:"}" ~with_:"\\}"
+    |> String.replace_all ~pattern:"\\" ~with_:"\\\\"
+  in
+  if not in_choice then
+    str
+  else
+    str
+    |> String.replace_all ~pattern:"," ~with_:"\\,"
+    |> String.replace_all ~pattern:"|" ~with_:"\\|"
+
+module IMap = Map.Make (struct
+  let compare = compare
+
+  type t = int
+end)
+
+type ctx = int * int IMap.t
+
+let pp_impl add_string (snippet : t) : unit =
+  let open Format in
+  let with_ctx ctx i_opt f : ctx =
+    let n, m = ctx in
+    match i_opt with
+    | None ->
+      add_string (f n);
+      (n + 1, m)
+    | Some i -> (
+      match IMap.find_opt i m with
+      | Some j ->
+        add_string (f j);
+        (n, m)
+      | None ->
+        add_string (f n);
+        (n + 1, IMap.add i n m) )
+  in
+  let rec go ctx = function
+    | Text s ->
+      add_string s;
+      ctx
+    | Concat (l, r) -> go (go ctx l) r
+    | Tabstop (i, `None) -> with_ctx ctx i (fun i -> sprintf "$%d" i)
+    | Tabstop (i, `Placeholder s) ->
+      let ctx = go (with_ctx ctx i (fun i -> sprintf "${%d:" i)) s in
+      add_string "}";
+      ctx
+    | Tabstop (i, `Choice values) ->
+      with_ctx ctx i (fun i ->
+          sprintf "${%d|%s|}" i
+            ( values
+            |> List.map ~f:(escape ~in_choice:true)
+            |> String.concat ~sep:"," ))
+    | Variable (var, opt) -> (
+      let var =
+        match var with
+        | TM_SELECTED_TEXT -> "TM_SELECTED_TEXT"
+        | TM_CURRENT_LINE -> "TM_CURRENT_LINE"
+        | TM_CURRENT_WORD -> "TM_CURRENT_WORD"
+        | TM_LINE_INDEX -> "TM_LINE_INDEX"
+        | TM_LINE_NUMBER -> "TM_LINE_NUMBER"
+        | TM_FILENAME -> "TM_FILENAME"
+        | TM_FILENAME_BASE -> "TM_FILENAME_BASE"
+        | TM_DIRECTORY -> "TM_DIRECTORY"
+        | TM_FILEPATH -> "TM_FILEPATH"
+      in
+      match opt with
+      | `None ->
+        add_string (sprintf "$%s" var);
+        ctx
+      | `Placeholder s ->
+        add_string (sprintf "${%s:" var);
+        let ctx = go ctx s in
+        add_string "}";
+        ctx
+      | `Transform t ->
+        sprintf "${%s/%s/%s/%s}" var t.regex t.format_string
+          (Option.value ~default:"" t.regex_options)
+        |> add_string;
+        ctx )
+  in
+  go (1, IMap.empty) snippet |> ignore
+
+let to_string t =
+  let buf = Buffer.create 0 in
+  pp_impl (Buffer.add_string buf) t;
+  Buffer.contents buf
+
+let pp_t (chan : Format.formatter) (t : t) : unit =
+  pp_impl (Format.pp_print_string chan) t
--- lsp/src/snippet.mli
+++ lsp/src/snippet.mli
@@ -1,33 +1,53 @@
 open! Import
 
-module Grammar: sig
-  type var =
-    | TM_SELECTED_TEXT
-    | TM_CURRENT_LINE
-    | TM_CURRENT_WORD
-    | TM_LINE_INDEX
-    | TM_LINE_NUMBER
-    | TM_FILENAME
-    | TM_FILENAME_BASE
-    | TM_DIRECTORY
-    | TM_FILEPATH
+type var =
+  | TM_SELECTED_TEXT
+  | TM_CURRENT_LINE
+  | TM_CURRENT_WORD
+  | TM_LINE_INDEX
+  | TM_LINE_NUMBER
+  | TM_FILENAME
+  | TM_FILENAME_BASE
+  | TM_DIRECTORY
+  | TM_FILEPATH
 
-  type variable_transform = {
-    regex: string;
-    format_string: string;
-    regex_options: string option
+type variable_transform =
+  { regex : string
+  ; format_string : string
+  ; regex_options : string option
   }
 
-  type t
+type t
 
-  val tabstop: int -> t
-  val placeholder: int -> t -> t
-  val choice: int -> string list -> t
-  val variable: ?opt:[`Placeholder of t | `Transform of variable_transform | `None] -> var -> t
-  val variable_transform: regex:string -> ?regex_options:string -> format_string:string -> variable_transform
-  val text: string -> t
+module Grammar : sig
+  val tabstop : int -> t
+
+  val placeholder : ?index:int -> t -> t
+
+  val choice : ?index:int -> string list -> t
+
+  val variable :
+       ?opt:[ `Placeholder of t | `Transform of variable_transform | `None ]
+    -> var
+    -> t
+
+  val variable_transform :
+       regex:string
+    -> ?regex_options:string
+    -> format_string:string
+    -> variable_transform
+
+  val text : string -> t
+
+  val ( ^^ ) : t -> t -> t
+
+  val ( @+ ) : string -> t -> t
+
+  val ( +@ ) : t -> string -> t
 end
 
-type t = Grammar.t
+val concat : ?sep:t -> t list -> t
 
-val to_string: t -> string
+val to_string : t -> string
+
+val pp_t : Format.formatter -> t -> unit
