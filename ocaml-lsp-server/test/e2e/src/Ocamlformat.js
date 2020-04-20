138dc8971b07f3e6e47a1d9cf423e780ca9e718c
BSKY <git@bsky.moe>
2020-04-20 19:15:45 +0900

    make fmt

--- ocaml-lsp-server/test/e2e/src/Ocamlformat.js
+++ ocaml-lsp-server/test/e2e/src/Ocamlformat.js
@@ -2,10 +2,10 @@
 
 var chunks = [];
 process.stdin.resume();
-process.stdin.on("data", function(chunk) {
+process.stdin.on("data", function (chunk) {
   chunks.push(chunk);
 });
-process.stdin.on("end", function() {
+process.stdin.on("end", function () {
   var body = Buffer.concat(chunks);
   if (body == "special string") {
     process.stderr.write("special string passed\n");
