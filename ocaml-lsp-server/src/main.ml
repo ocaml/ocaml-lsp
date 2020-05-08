fe045e281831a3d9df0257812b4b8692e60f7aaf
Rusty Key <iam@stfoo.ru>
2020-05-08 19:32:44 +0300

    introduce refmt-width argument

--- ocaml-lsp-server/src/main.ml
+++ ocaml-lsp-server/src/main.ml
@@ -1,4 +1,6 @@
-let run log_file = Ocaml_lsp_server.run ~log_file
+let run log_file _refmt_width = Ocaml_lsp_server.run ~log_file
+
+let default_refmt_width = 80
 
 let () =
   let open Cmdliner in
@@ -11,9 +13,15 @@ let () =
     value & opt (some string) None & info [ "log-file" ] ~docv:"FILE" ~doc ~env
   in
 
+  let refmt_width =
+    let open Arg in
+    let doc = "refmt wrapping width" in
+    value & opt int default_refmt_width & info ["w"; "refmt-width"] ~docv:"WIDTH" ~doc
+  in
+
   let cmd =
     let doc = "Start OCaml LSP server (only stdio transport is supported)" in
-    ( Term.(const run $ log_file)
+    ( Term.(const run $ log_file $ refmt_width)
     , Term.info "ocamllsp" ~doc ~exits:Term.default_exits )
   in
 
