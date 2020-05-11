b1038d653631083a3ee4f3d1da970603d7e10cab
Rudi Grinberg <me@rgrinberg.com>
2020-05-10 18:28:44 -0700

    WIP symmetric handling of clients/servers
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/src/rpc.ml
+++ lsp/src/rpc.ml
@@ -95,3 +95,129 @@ module Io = struct
       | r -> Ok r
       | exception _exn -> Error "Unexpected packet")
 end
+
+type client = Client
+
+type server = Server
+
+type (_, 'resp) request =
+  | Client : 'resp Client_request.t -> (client, 'resp) request
+  | Server : 'resp Server_request.t -> (server, 'resp) request
+
+type _ notification =
+  | Client : Client_notification.t -> client notification
+  | Server : Server_notification.t -> server notification
+
+module Handler = struct
+  type 'a on_request = E : (('a, 'r) request -> 'r Fiber.t) -> 'a on_request
+
+  type 'a gen =
+    { on_request : 'a on_request
+    ; on_notification : 'a notification -> unit
+    }
+
+  (* A server has a [client handler] A client has a [server handler] *)
+  type _ t =
+    | Client : server gen -> client t
+    | Server : client gen -> server t
+
+  let on_notification_default _ =
+    (* TODO we can also just log it *)
+    ()
+
+  let client ?on_request ?(on_notification = on_notification_default) () =
+    let on_request : server on_request =
+      let f =
+        match on_request with
+        | Some s -> s
+        | None -> assert false
+      in
+      E (fun (Server x) -> f x)
+    in
+    let on_notification : server notification -> unit =
+     fun (Server x) -> on_notification x
+    in
+    Client { on_request; on_notification }
+
+  let server ?on_request ?(on_notification = on_notification_default) () =
+    let on_request : client on_request =
+      let f =
+        match on_request with
+        | Some s -> s
+        | None -> assert false
+      in
+      E (fun (Client x) -> f x)
+    in
+    let on_notification : client notification -> unit =
+     fun (Client x) -> on_notification x
+    in
+    Server { on_request; on_notification }
+end
+
+module State = struct
+  type t =
+    | Waiting_for_init
+    | Running
+    | Closed
+end
+
+type 'h t =
+  { handler : 'h Handler.t
+  ; io : Io.t
+  ; mutable state : State.t
+  }
+
+let make handler io = { handler; io; state = Waiting_for_init }
+
+let req_id = ref 1
+
+let request (type a r) (t : a t) (req : (a, r) request) : r Fiber.t =
+  let id = Either.Right !req_id in
+  let jsonrpc_request =
+    incr req_id;
+    match req with
+    | Client r -> Client_request.to_jsonrpc_request r ~id
+    | Server r -> Server_request.to_jsonrpc_request r ~id
+  in
+  let open Fiber.O in
+  let* () = Io.send t.io (Request jsonrpc_request) in
+  let* resp = Io.read_response t.io in
+  match resp with
+  | Error e -> failwith e
+  | Ok r -> (
+    assert (r.id = id);
+    match r.result with
+    | Error e -> Jsonrpc.Response.Error.raise e
+    | Ok (json : Json.t) ->
+      Fiber.return
+        ( match req with
+        | Client r -> Client_request.response_of_json r json
+        | Server r -> Server_request.response_of_json r json ) )
+
+let notification (type a) (t : a t) (n : a notification) : unit Fiber.t =
+  let jsonrpc_request =
+    match n with
+    | Client r -> Client_notification.to_jsonrpc_request r
+    | Server r -> Server_notification.to_jsonrpc_request r
+  in
+  Io.send t.io (Request jsonrpc_request)
+
+let start_loop _ = ()
+
+let start_client (t : client t) (p : Types.InitializeParams.t) =
+  assert (t.state = Waiting_for_init);
+  let open Fiber.O in
+  let+ resp = request t (Client (Client_request.Initialize p)) in
+  t.state <- Running;
+  start_loop t;
+  resp
+
+let start_server t = start_loop t
+
+let initialized _ = assert false
+
+let stop t =
+  t.state <- Closed;
+  assert false
+
+let stopped _ = assert false
--- lsp/src/rpc.mli
+++ lsp/src/rpc.mli
@@ -28,3 +28,49 @@ module Io : sig
 
   val read_response : t -> (Response.t, string) result Fiber.t
 end
+
+type client = Client
+
+type server = Server
+
+type (_, 'resp) request =
+  | Client : 'resp Client_request.t -> (client, 'resp) request
+  | Server : 'resp Server_request.t -> (server, 'resp) request
+
+type _ notification =
+  | Client : Client_notification.t -> client notification
+  | Server : Server_notification.t -> server notification
+
+module Handler : sig
+  type _ t
+
+  val server :
+       ?on_request:('a Client_request.t -> 'a Fiber.t)
+    -> ?on_notification:(Client_notification.t -> unit)
+    -> unit
+    -> server t
+
+  val client :
+       ?on_request:('a Server_request.t -> 'a Fiber.t)
+    -> ?on_notification:(Server_notification.t -> unit)
+    -> unit
+    -> client t
+end
+
+type _ t
+
+val make : 'a Handler.t -> Io.t -> 'a t
+
+val stop : _ t -> unit
+
+val start_client : client t -> Types.InitializeParams.t -> Types.InitializeResult.t Fiber.t
+
+val start_server : server t -> unit
+
+val stopped : _ t -> unit Fiber.t
+
+val initialized : _ t -> unit Fiber.t
+
+val request : 'a t -> ('a, 'resp) request -> 'resp Fiber.t
+
+val notification : 'a t -> 'a notification -> unit Fiber.t
