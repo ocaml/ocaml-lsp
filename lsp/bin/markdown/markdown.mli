04f5f940766b843fbbe1926ae74b54457c1244df
Sora Morimoto <git@bsky.moe>
2020-05-18 19:35:03 +0900

    make fmt

--- lsp/bin/markdown/markdown.mli
+++ lsp/bin/markdown/markdown.mli
@@ -1,5 +1,5 @@
-open! Import
 (** Trivial parser to extract ```typescript sections out of markdown docs. *)
+open! Import
 
 (** Extracts all typescript sections *)
 val read_typescript : Lexing.lexbuf -> string list
