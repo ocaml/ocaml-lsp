c3ba08086c2840696bd2993733fe87de0017e0ff
Rudi Grinberg <me@rgrinberg.com>
2020-05-18 20:26:39 -0700

    fix jsonrpc/fiber tests
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/jsonrpc_test.ml
+++ lsp/test/jsonrpc_test.ml
@@ -18,6 +18,7 @@ module Session = Jsonrpc.Session (Stream_chan)
 let print_json json = print_endline (Yojson.Safe.pretty_to_string json)
 
 let () =
+  Printexc.record_backtrace true;
   let response =
     let i = ref 0 in
     fun () ->
@@ -30,25 +31,39 @@ let () =
   in
   let on_notification n =
     let json = Request.yojson_of_t n in
+    print_endline ">> received notification";
     print_json json;
     Fiber.return ()
   in
   let responses = ref [] in
+  let initial_requests =
+    [ Request (Jsonrpc.Request.create ~id:(Right 10) ~method_:"foo" ())
+    ; Request
+        (Jsonrpc.Request.create ~params:`Null ~id:(Left "testing")
+           ~method_:"bar" ())
+    ; Request (Jsonrpc.Request.create ~params:`Null ~method_:"notif1" ())
+    ; Request (Jsonrpc.Request.create ~params:`Null ~method_:"notif2" ())
+    ]
+  in
+  let reqs_in, reqs_out = Fiber_stream.pipe () in
   let chan =
-    let in_ =
-      In.of_list
-        [ Request (Jsonrpc.Request.create ~id:(Right 10) ~method_:"foo" ())
-        ; Request
-            (Jsonrpc.Request.create ~params:`Null ~id:(Left "testing")
-               ~method_:"bar" ())
-        ]
-    in
     let out = Out.of_ref responses in
-    (in_, out)
+    (reqs_in, out)
   in
   let session = Session.create ~on_notification ~on_request chan in
   let scheduler = Scheduler.create () in
-  let () = Scheduler.run scheduler (Session.run session) in
+  let write_reqs () =
+    let open Fiber.O in
+    let* () =
+      Fiber.sequential_iter initial_requests ~f:(fun req ->
+          Fiber_stream.Out.write reqs_out (Some req))
+    in
+    Fiber_stream.Out.write reqs_out None
+  in
+  let (), () =
+    Scheduler.run scheduler
+      (Fiber.fork_and_join write_reqs (fun () -> Session.run session))
+  in
   List.rev !responses
   |> List.iter ~f:(fun packet ->
          let json =
