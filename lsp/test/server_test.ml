af2fd885995f0cd36aec273c52b977dfed0d8c8f
Rudi Grinberg <me@rgrinberg.com>
2020-05-25 14:17:56 -0700

    Add e2e client/server test
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/server_test.ml
+++ lsp/test/server_test.ml
@@ -1,83 +1,93 @@
 open Lsp
 open! Import
 
+let scheduler = Scheduler.create ()
+
 module Client = struct
-  let on_request _ _ =
-    let code = Jsonrpc.Response.Error.Code.InternalError in
-    let message = "Request not supported" in
-    Fiber.return (Error (Jsonrpc.Response.Error.make ~code ~message ()))
+  let on_request _ =
+    Format.eprintf "fix me@.%!";
+    failwith "not implemented"
 
-  let on_notification _ _notif = Format.eprintf "Received notification@.%!"
+  let on_notification _ = Format.eprintf "client: received notification@.%!"
 
-  let handler = { Client.on_request; on_notification }
+  let handler =
+    let on_request = { Client.Handler.on_request } in
+    Client.Handler.make ~on_request ~on_notification ()
 
   let run in_ out =
     let initialize =
       let capabilities = Types.ClientCapabilities.create () in
       Types.InitializeParams.create ~capabilities ()
     in
-    let client = Client.create handler in_ out initialize in
-    let start () =
-      let _start = Client.start client in
-      let open Fiber.O in
-      let+ (_ : Types.InitializeResult.t) = Client.initialized client in
-      Format.eprintf "initialized"
+    let client =
+      let io = Rpc.Io.make in_ out in
+      let stream_io = Rpc.Stream_io.make scheduler io in
+      Client.make handler stream_io
     in
-    Thread.create start ()
+    let running = Client.start client initialize in
+    let open Fiber.O in
+    let* init =
+      Fiber.fork (fun () ->
+          let+ (_ : Types.InitializeResult.t) = Client.initialized client in
+          Format.eprintf "client: initialized server@.%!")
+    in
+    Scheduler.detach scheduler (fun () -> Fiber.Future.wait init);
+    running
 end
 
 module Server = struct
+  open Lsp.Types
+  module Server = Rpc.Server
+
   type state =
     | Started
     | Initialized
 
-  let on_initialize _ s _ =
-    assert (s = Started);
-    let ir =
-      let capabilities = Types.ServerCapabilities.create () in
-      Types.InitializeResult.create ~capabilities ()
+  let on_request state =
+    let on_request (type a) (req : a Client_request.t) : a Fiber.t =
+      match req with
+      | Client_request.Initialize _ ->
+        state := Initialized;
+        let capabilities = ServerCapabilities.create () in
+        let result = InitializeResult.create ~capabilities () in
+        Format.eprintf "server: initializing server@.";
+        Fiber.return result
+      | _ -> failwith "not supported"
     in
-    Format.eprintf "initialized server@.%!";
-    Fiber.return (Ok (Initialized, ir))
+    { Server.Handler.on_request }
 
-  let on_request _ state _ _ =
-    assert (state = Initialized);
-    let code = Jsonrpc.Response.Error.Code.InternalError in
-    let message = "Request not supported" in
-    Fiber.return (Error (Jsonrpc.Response.Error.make ~code ~message ()))
+  let on_notification _ = Format.eprintf "server: Received notification@.%!"
 
-  let on_notification _ state _notif =
-    assert (state = Initialized);
-    Format.eprintf "Received notification@.%!";
-    Fiber.return (Ok state)
-
-  let handler = { Server.on_initialize; on_request; on_notification }
+  let handler state =
+    let on_request = on_request state in
+    Server.Handler.make ~on_request ~on_notification ()
 
   let run in_ out =
-    let loop () = Server.start Started handler in_ out |> Fiber.run in
-    Thread.create loop ()
+    let state = ref Started in
+    let server =
+      let io = Rpc.Io.make in_ out in
+      let stream_io = Rpc.Stream_io.make scheduler io in
+      Server.make (handler state) stream_io
+    in
+    Server.start server
 end
 
+let pipe () =
+  let in_, out = Unix.pipe () in
+  (Unix.in_channel_of_descr in_, Unix.out_channel_of_descr out)
+
 let () =
-  let client_in, server_out = Unix.pipe () in
-  let server_in, client_out = Unix.pipe () in
-  let server_thread =
-    let in_ = Unix.in_channel_of_descr server_in in
-    let out = Unix.out_channel_of_descr server_out in
-    Server.run in_ out
-  in
-  let client_thread =
-    let in_ = Unix.in_channel_of_descr client_in in
-    let out = Unix.out_channel_of_descr client_out in
-    Client.run in_ out
-  in
+  let client_in, server_out = pipe () in
+  let server_in, client_out = pipe () in
+  let server () = Server.run server_in server_out in
+  let client () = Client.run client_in client_out in
   let (_ : Thread.t) =
     Thread.create
       (fun () ->
-        Thread.delay 5.0;
-        Format.eprintf "Test failed to terminate before 5 seconds";
+        let delay = 3.0 in
+        Thread.delay delay;
+        Format.eprintf "Test failed to terminate before %f seconds" delay;
         exit 1)
       ()
   in
-  Thread.join server_thread;
-  Thread.join client_thread
+  Scheduler.run scheduler (Fiber.fork_and_join_unit client server)
