ea405a6147ef88a9d5404b3efec33239edb809f5
Rudi Grinberg <me@rgrinberg.com>
2020-05-26 22:55:43 -0700

    Add logging module
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/src/import.ml
+++ lsp/src/import.ml
@@ -308,4 +308,30 @@ module Fiber = struct
   end
 end
 
+module Log = struct
+  let level : (string option -> bool) ref = ref (fun _ -> false)
+
+  let out = ref Format.err_formatter
+
+  type message =
+    { message : string
+    ; payload : (string * Json.t) list
+    }
+
+  let msg message payload = { message; payload }
+
+  let log ?section k =
+    if !level section then (
+      let message = k () in
+      ( match section with
+      | None -> Format.fprintf !out "%s@." message.message
+      | Some section -> Format.fprintf !out "[%s] %s@." section message.message
+      );
+      ( match message.payload with
+      | [] -> ()
+      | fields -> Format.fprintf !out "%a@." Json.pp (`Assoc fields) );
+      Format.pp_print_flush !out ()
+    )
+end
+
 let sprintf = Stdune.sprintf
