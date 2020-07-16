385c9aaea20dde5e48d8306972b9071905287c85
Ulugbek Abdullaev <ulugbekna@gmail.com>
2020-07-16 14:46:41 +0200

    fix docs fetch (before dot) test

--- ocaml-lsp-server/test/e2e/__tests__/completionItem-resolve.test.ts
+++ ocaml-lsp-server/test/e2e/__tests__/completionItem-resolve.test.ts
@@ -86,13 +86,19 @@ describe("textDocument/completion", () => {
     `);
 
     let response = await queryCompletionItemResolve(
-      "find_all",
+      "LargeFile",
       Types.Position.create(0, 15),
     );
 
     expect(response).toMatchInlineSnapshot(`
       Object {
-        "label": "find_all",
+        "documentation": " Operations on large files.
+        This sub-module provides 64-bit variants of the channel functions
+        that manipulate file positions and file sizes.  By representing
+        positions and sizes by 64-bit integers (type [int64]) instead of
+        regular integers (type [int]), these alternate functions allow
+        operating on files whose sizes are greater than [max_int]. ",
+        "label": "LargeFile",
       }
     `);
   });
