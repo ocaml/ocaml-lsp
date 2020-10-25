4ae5d688d1e2339d53d1f7025f43edea3f67d6b8
Rudi Grinberg <me@rgrinberg.com>
2020-10-25 16:52:04 -0700

    Move common test code to Import
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/fiber_stream_tests.ml
+++ lsp/test/fiber_stream_tests.ml
@@ -1,49 +1,5 @@
-open Lsp
-open Stdune
-open! Fiber.O
-open Dyn.Encoder
-
-let printf = Printf.printf
-
-let print pp = Format.printf "%a@." Pp.render_ignore_tags pp
-
-let print_dyn dyn = print (Dyn.pp dyn)
-
-module Scheduler : sig
-  exception Never
-
-  val yield : unit -> unit Fiber.t
-
-  val run : 'a Fiber.t -> 'a
-end = struct
-  let suspended = Queue.create ()
-
-  let yield () =
-    let ivar = Fiber.Ivar.create () in
-    Queue.push suspended ivar;
-    Fiber.Ivar.read ivar
-
-  let rec restart_suspended () =
-    if Queue.is_empty suspended then
-      Fiber.return ()
-    else
-      let* () = Fiber.Ivar.fill (Queue.pop_exn suspended) () in
-      restart_suspended ()
-
-  exception Never
-
-  let run t =
-    match
-      Fiber.run
-        (let* result = Fiber.fork (fun () -> t) in
-         let* () = restart_suspended () in
-         Fiber.Ivar.peek result)
-    with
-    | None
-    | Some None ->
-      raise Never
-    | Some (Some x) -> x
-end
+open Import
+open Fiber_stream
 
 let failing_fiber () : unit Fiber.t =
   Scheduler.yield () >>= fun () -> raise Exit
@@ -56,21 +12,6 @@ let long_running_fiber () =
       Scheduler.yield () >>= fun () -> loop (n - 1)
   in
   loop 10
-
-let test ?(expect_never = false) to_dyn f =
-  let never_raised = ref false in
-  ( try Scheduler.run f |> to_dyn |> print_dyn
-    with Scheduler.Never -> never_raised := true );
-  match (!never_raised, expect_never) with
-  | false, false ->
-    (* We don't raise in this case b/c we assume something else is being tested *)
-    ()
-  | true, true -> print_endline "[PASS] Never raised as expected"
-  | false, true ->
-    print_endline "[FAIL] expected Never to be raised but it wasn't"
-  | true, false -> print_endline "[FAIL] unexpected Never raised"
-
-open Fiber_stream
 
 let%expect_test "of_list & iter" =
   test unit
