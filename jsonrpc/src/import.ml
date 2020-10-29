f573c1dd69a4121a2b6aa4931ee23179ba3a5187
Rudi Grinberg <me@rgrinberg.com>
2020-10-29 12:11:13 -0700

    Remove unused functions in import.ml
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- jsonrpc/src/import.ml
+++ jsonrpc/src/import.ml
@@ -8,21 +8,7 @@ end
 module Json = struct
   type t = Yojson.Safe.t
 
-  let to_pretty_string (t : t) = Yojson.Safe.pretty_to_string ~std:false t
-
-  let to_string t = Yojson.Safe.to_string t
-
-  let of_string s = Yojson.Safe.from_string s
-
-  let yojson_of_t x = x
-
-  let t_of_yojson x = x
-
   let error = Ppx_yojson_conv_lib.Yojson_conv.of_yojson_error
-
-  let yojson_of_list = Ppx_yojson_conv_lib.Yojson_conv.yojson_of_list
-
-  let pp ppf (t : t) = Yojson.Safe.pretty_print ppf t
 
   module Jsonable = Ppx_yojson_conv_lib.Yojsonable
 
@@ -33,26 +19,5 @@ module Json = struct
     | Some f -> f
     | None -> error "Jsonrpc.Result.t: missing field" (`Assoc fields)
 
-  module Conv = struct
-    include Ppx_yojson_conv_lib.Yojson_conv
-  end
-
-  module O = struct
-    let ( <|> ) c1 c2 json =
-      match c1 json with
-      | s -> s
-      | exception Conv.Of_yojson_error (_, _) -> c2 json
-  end
-
-  module Option = struct
-    type 'a t = 'a option
-
-    let yojson_of_t f = function
-      | None -> `Null
-      | Some x -> f x
-
-    let t_of_yojson f = function
-      | `Null -> None
-      | json -> Some (f json)
-  end
+  module Conv = Ppx_yojson_conv_lib.Yojson_conv
 end
