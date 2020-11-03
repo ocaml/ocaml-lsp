83b786ce820348f5f1c0940ba7776e10b100beba
Rudi Grinberg <me@rgrinberg.com>
2020-11-02 19:24:01 -0800

    move mvar to own module
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- fiber-unix/src/scheduler.ml
+++ fiber-unix/src/scheduler.ml
@@ -1,39 +1,5 @@
 open Import
 
-module Mvar : sig
-  type 'a t
-
-  val create : unit -> 'a t
-
-  val get : 'a t -> 'a
-
-  val set : 'a t -> 'a -> unit
-end = struct
-  type 'a t =
-    { m : Mutex.t
-    ; cv : Condition.t
-    ; mutable cell : 'a option
-    }
-
-  let create () = { m = Mutex.create (); cv = Condition.create (); cell = None }
-
-  let get t =
-    let rec await_value t =
-      match t.cell with
-      | None ->
-        Condition.wait t.cv t.m;
-        await_value t
-      | Some v ->
-        t.cell <- None;
-        v
-    in
-    with_mutex t.m ~f:(fun () -> await_value t)
-
-  let set t v =
-    with_mutex t.m ~f:(fun () -> t.cell <- Some v);
-    Condition.signal t.cv
-end
-
 module Worker : sig
   (** Simple queue that is consumed by its own thread *)
   type 'work t
