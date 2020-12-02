44272df0293c77031a64b745925fc1af8f2b1cd2
Thibaut <thibaut.mattio@gmail.com>
2020-12-02 15:19:12 +0100

    Use select to check if data is available on the fd before reading

--- fiber-unix/src/barrier.ml
+++ fiber-unix/src/barrier.ml
@@ -6,40 +6,26 @@ type t =
 
 let create () =
   let r, w = Unix.pipe () in
-  Unix.set_nonblock r;
-  { r; w; buf = Bytes.create 8 }
+  { r; w; buf = Bytes.create 1 }
 
 let close t =
   Unix.close t.w;
   Unix.close t.r
 
-let read_one t =
-  match Unix.read t.r t.buf 0 (Bytes.length t.buf) with
-  | 0 -> Error `No_data
-  | _ -> Ok ()
-  | exception Unix.Unix_error (Unix.EAGAIN, _, _) -> Error `Unavailable
-  | exception Unix.Unix_error (Unix.EBADF, _, _) -> Error `Bad_fd
-  | exception Unix.Unix_error (e, _, _) -> failwith (Unix.error_message e)
+let rec empty_fd fd buf =
+  match Unix.select [ fd ] [] [] 0.0 with
+  | [], _, _ -> Ok ()
+  | _ ->
+    let _ = Unix.read fd buf 0 1 in
+    empty_fd fd buf
+  | exception Unix.Unix_error (Unix.EBADF, _, _) -> Error (`Closed (`Read true))
+  | exception Unix.Unix_error (e, _, _) ->
+    failwith ("read :" ^ Unix.error_message e)
 
-let read_all t =
-  let rec loop read_once =
-    match read_one t with
-    | Ok () -> loop true
-    | Error `Bad_fd -> Error (`Bad_fd, read_once)
-    | Error `Unavailable
-    | Error `No_data ->
-      Ok read_once
-  in
-  loop false
-
-let rec await ?(timeout = -1.) t =
+let await ?(timeout = -1.) t =
   match Unix.select [ t.r ] [] [] timeout with
   | [], _, _ -> Error `Timeout
-  | _ -> (
-    match read_all t with
-    | Ok true -> Ok ()
-    | Ok false -> await ~timeout t
-    | Error (`Bad_fd, read_once) -> Error (`Closed (`Read read_once)) )
+  | _ -> empty_fd t.r t.buf
   | exception Unix.Unix_error (Unix.EBADF, _, _) ->
     Error (`Closed (`Read false))
   | exception Unix.Unix_error (e, _, _) ->
