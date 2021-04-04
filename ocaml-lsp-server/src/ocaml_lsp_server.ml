f1c1f476f042538004136c9786e79a208d3d470f
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-04-04 12:00:16 +0500

    simplify diagnostics sending code:   we don't need `Document.with_pipeline_exn` if we use   `Document.with_pipeline_exn`

--- ocaml-lsp-server/src/ocaml_lsp_server.ml
+++ ocaml-lsp-server/src/ocaml_lsp_server.ml
@@ -134,21 +134,20 @@ let send_diagnostics ?diagnostics rpc doc =
               Query_protocol.Errors
                 { lexing = true; parsing = true; typing = true }
             in
-            Document.with_pipeline_exn doc (fun pipeline ->
-                let errors = Query_commands.dispatch pipeline command in
-                List.map errors ~f:(fun (error : Loc.error) ->
-                    let loc = Loc.loc_of_report error in
-                    let range = Range.of_loc loc in
-                    let severity =
-                      match error.source with
-                      | Warning -> DiagnosticSeverity.Warning
-                      | _ -> DiagnosticSeverity.Error
-                    in
-                    let message =
-                      Loc.print_main Format.str_formatter error;
-                      String.trim (Format.flush_str_formatter ())
-                    in
-                    create_diagnostic range message ~severity))
+            let+ errors = Document.dispatch_exn doc command in
+            List.map errors ~f:(fun (error : Loc.error) ->
+                let loc = Loc.loc_of_report error in
+                let range = Range.of_loc loc in
+                let severity =
+                  match error.source with
+                  | Warning -> DiagnosticSeverity.Warning
+                  | _ -> DiagnosticSeverity.Error
+                in
+                let message =
+                  Loc.print_main Format.str_formatter error;
+                  String.trim (Format.flush_str_formatter ())
+                in
+                create_diagnostic range message ~severity)
           in
           let notif = create_publishDiagnostics uri diagnostics in
           Server.notification rpc notif))
