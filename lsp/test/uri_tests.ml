71f07ea8bed49d7f87a5dd10d36977c4a2f9ca7c
Rudi Grinberg <me@rgrinberg.com>
2021-04-26 17:29:12 -0700

    Fix URI handling on Windows
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/uri_tests.ml
+++ lsp/test/uri_tests.ml
@@ -4,4 +4,22 @@ let%expect_test "test uri parsing" =
   let uri = Uri.t_of_yojson (`String "file:///Users/foo") in
   print_endline (Uri.to_path uri);
   [%expect {|
-    /Users/foo |}]
+    /Users/foo |}];
+  print_endline (Uri.to_string uri);
+  [%expect {| file:///Users/foo |}];
+  let uri = Uri.t_of_yojson (`String "file:///c:/Users/foo") in
+  print_endline (Uri.to_path uri);
+  [%expect {| /c:/Users/foo |}];
+  print_endline (Uri.to_string uri);
+  [%expect {|
+    file:///c:/Users/foo |}]
+
+let%expect_test "uri of path" =
+  let uri = Uri.of_path "/foo/bar.ml" in
+  print_endline (Uri.to_string uri);
+  [%expect {|
+    file:///foo/bar.ml |}];
+  let uri = Uri.of_path "foo/bar.mli" in
+  print_endline (Uri.to_string uri);
+  [%expect {|
+    file:///foo/bar.mli |}]
