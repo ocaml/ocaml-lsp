9304a8088ffbd3b2169e2479711337fbcb293b42
Rudi Grinberg <me@rgrinberg.com>
2021-05-05 01:15:34 -0700

    [nix] remove more redunancy
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- nix/default.nix
+++ nix/default.nix
@@ -1,15 +1,14 @@
 # paramaterised derivation with dependencies injected (callPackage style)
 { pkgs, stdenv, opam2nix }:
 let
+  strings = pkgs.lib.strings;
   args = {
     inherit (pkgs.ocaml-ng.ocamlPackages_4_12) ocaml;
     selection = ./opam-selection.nix;
     src = ../.;
   };
   opam-selection = opam2nix.build args;
-  localPackages = let
-    strings = pkgs.lib.strings;
-    contents = builtins.attrNames (builtins.readDir ../.);
+  localPackages = let contents = builtins.attrNames (builtins.readDir ../.);
   in builtins.filter (strings.hasSuffix ".opam") contents;
   resolve = opam2nix.resolve args (localPackages ++ [
     # dev deps
@@ -22,10 +21,12 @@ let
     "ppx_expect"
   ]);
 
-in {
-  inherit (opam-selection) jsonrpc; # jsonrpc = opam-selection.jsonrpc
-  inherit (opam-selection) lsp;
-  inherit (opam-selection) ocaml-lsp-server;
-  inherit resolve;
-  opam = opam-selection;
-}
+in (builtins.listToAttrs (builtins.map (fname:
+  let packageName = strings.removeSuffix ".opam" fname;
+  in {
+    name = packageName;
+    value = builtins.getAttr packageName opam-selection;
+  }) localPackages)) // {
+    inherit resolve;
+    opam = opam-selection;
+  }
