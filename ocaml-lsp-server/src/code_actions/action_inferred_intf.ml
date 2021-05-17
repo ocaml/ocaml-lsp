b06cbc483dd9b60784913593f218113bb5873c89
Rudi Grinberg <me@rgrinberg.com>
2021-05-17 13:08:52 -0700

    Change inferred intf to use versioned edits
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/code_actions/action_inferred_intf.ml
+++ ocaml-lsp-server/src/code_actions/action_inferred_intf.ml
@@ -2,10 +2,18 @@ open Import
 
 let action_kind = "inferred_intf"
 
-let code_action_of_intf uri intf range =
+let code_action_of_intf doc intf range =
   let edit : WorkspaceEdit.t =
-    let textedit : TextEdit.t = { range; newText = intf } in
-    WorkspaceEdit.create ~changes:[ (uri, [ textedit ]) ] ()
+    let edit =
+      let textedit : TextEdit.t = { range; newText = intf } in
+      let textDocument =
+        let uri = Document.uri doc in
+        let version = Document.version doc in
+        OptionalVersionedTextDocumentIdentifier.create ~uri ~version ()
+      in
+      TextDocumentEdit.create ~textDocument ~edits:[ `TextEdit textedit ]
+    in
+    WorkspaceEdit.create ~documentChanges:[ `TextDocumentEdit edit ] ()
   in
   let title = String.capitalize_ascii "Insert inferred interface" in
   CodeAction.create ~title ~kind:(CodeActionKind.Other action_kind) ~edit
@@ -17,4 +25,4 @@ let code_action doc (state : State.t) (params : CodeActionParams.t) =
   | Impl -> Fiber.return None
   | Intf ->
     let+ intf = Inference.infer_intf ~force_open_impl:true state doc in
-    Some (code_action_of_intf (Document.uri doc) intf params.range)
+    Some (code_action_of_intf doc intf params.range)
