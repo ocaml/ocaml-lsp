8c7e184a106b477a22dd51a13deafbdd5c8ca414
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-06-14 14:21:31 +0500

    Add a compare function for `Range.t`

--- ocaml-lsp-server/src/range.mli
+++ ocaml-lsp-server/src/range.mli
@@ -2,6 +2,10 @@ open Import
 
 include module type of Lsp.Types.Range with type t = Lsp.Types.Range.t
 
+(** [compare r1 r2] compares first start positions, if equal compares the end
+    positions. *)
+val compare : t -> t -> Ordering.t
+
 val compare_size : t -> t -> Ordering.t
 
 val first_line : t
