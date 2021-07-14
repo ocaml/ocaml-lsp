3c4538c661698b7f37983bba08a4d59861bd0435
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-07-14 19:54:24 +0500

    make `notify-if-no-hole` mandatory

--- ocaml-lsp-server/src/client.ml
+++ ocaml-lsp-server/src/client.ml
@@ -9,19 +9,18 @@ module Vscode = struct
     module Custom : sig
       val next_hole :
            ?start_position:Position.t
-        -> ?notify_if_no_hole:bool
+        -> notify_if_no_hole:bool
         -> unit
         -> Command.t
     end = struct
-      let next_hole ?start_position ?notify_if_no_hole () =
+      let next_hole ?start_position ~notify_if_no_hole () =
         let arguments =
           let pos_json =
             Option.map start_position ~f:(fun p ->
                 ("position", Position.yojson_of_t p))
           in
           let notif_json =
-            Option.map notify_if_no_hole ~f:(fun b ->
-                ("notify-if-no-hole", Json.bool b))
+            Some ("notify-if-no-hole", Json.bool notify_if_no_hole)
           in
           let arg_obj_fields =
             List.filter_map [ pos_json; notif_json ] ~f:Fun.id
