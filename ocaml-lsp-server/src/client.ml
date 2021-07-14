618618dceaa014e91b17c6f7d7a939e50914bcdb
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-07-14 13:01:22 +0500

    add an interface for `client.ml`

--- ocaml-lsp-server/src/client.ml
+++ ocaml-lsp-server/src/client.ml
@@ -1,26 +1,12 @@
 open Import
 
 module Vscode = struct
-  (** A collection of VS Code editor commands.
-
-      Reference for VS Code built-in commands:
-      https://code.visualstudio.com/api/references/commands *)
   module Commands = struct
-    (** [editor.action.triggerSuggest] is a vscode-specific command, which
-        triggers the completion request on all completion providers *)
     let triggerSuggest =
       Command.create ~title:"Trigger Suggest"
         ~command:"editor.action.triggerSuggest" ()
 
-    (** Represents custom commands, i.e., commands added by an extension.
-
-        Currently, the module includes custom commands introduced by "Ocaml
-        Platform" extension *)
     module Custom : sig
-      (** Request client cursor to jump to the next hole. Looks for a hole
-          starting at position [start_position], if provided; otherwise, uses
-          the cursor position. Will not show a pop-up notification if
-          [notify-if-no-hole] is set to [false] (the default value is [true]) *)
       val next_hole :
            ?start_position:Position.t
         -> ?notify_if_no_hole:bool
---
+++ ocaml-lsp-server/src/client.mli
@@ -0,0 +1,35 @@
+open Import
+
+(** This module is a collection of client-specific functionality (client =
+    editor) *)
+
+module Vscode : sig
+  (** A collection of VS Code editor commands.
+
+      Reference for VS Code built-in commands:
+      https://code.visualstudio.com/api/references/commands *)
+  module Commands : sig
+    (** [editor.action.triggerSuggest] is a vscode-specific command, which
+        triggers the completion request on all completion providers *)
+    val triggerSuggest : Command.t
+
+    (** Represents custom commands, i.e., commands added by a certain extension.
+
+        Currently, the module includes custom commands introduced by "OCaml
+        Platform" extension *)
+    module Custom : sig
+      (** Request client cursor to jump to the next hole.
+
+          Looks for a hole starting at position [start_position], if provided;
+          otherwise, uses the cursor position.
+
+          Will not show a pop-up notification if [notify-if-no-hole] is set to
+          [false] (the default value is [true]) *)
+      val next_hole :
+           ?start_position:Position.t
+        -> ?notify_if_no_hole:bool
+        -> unit
+        -> Command.t
+    end
+  end
+end
