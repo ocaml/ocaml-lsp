baa2c74fde00f692491029f5b1fcf44df81d2ad2
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-08-08 20:58:45 +0500

    create a `Channel` for thread-safe message passing

---
+++ fiber-unix/src/channel.mli
@@ -0,0 +1,24 @@
+type 'a t
+
+type elt_in_channel
+
+val create : unit -> 'a t
+
+(** [send_removable ch v] puts a value [v] in the channel [ch] in a non-blocking
+    manner (we consider acquiring/waiting for the lock to be "non-blocking").
+    The returned value [elt_in_channel] can then be used to remove the put
+    element from the channel if it hasn't been consumed by the other end of the
+    channel yet. *)
+val send_removable : 'a t -> 'a -> (elt_in_channel, [ `Closed ]) result
+
+(** similar to [put_removable] but the returned value is ignored *)
+val send : 'a t -> 'a -> (unit, [ `Closed ]) result
+
+(** remove element put in the channel if it hasn't been consumed yet *)
+val remove_if_not_consumed : elt_in_channel -> unit
+
+(** [get ch] reads a value [v] from the channel [ch]. If there is no value to
+    read, the thread sleeps until there is some value to read. *)
+val get : 'a t -> ('a, [ `Closed ]) result
+
+val close : 'a t -> (unit, [ `Already_closed ]) result
--- fiber-unix/src/fiber_unix.ml
+++ fiber-unix/src/fiber_unix.ml
@@ -1,5 +1,6 @@
 module Scheduler = Scheduler
 
 module Private = struct
+  module Channel = Channel
   module Removable_queue = Removable_queue
 end
