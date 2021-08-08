027159bf253ef30af278be522857c2046e92a5eb
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-08-08 21:07:49 +0500

    clean up `Worker` impl; it seems we don't use the thread handle

--- fiber-unix/src/worker.ml
+++ fiber-unix/src/worker.ml
@@ -1,13 +1,10 @@
 open Import
 
-type 'a t =
-  { work_chan : 'a Channel.t
-  ; mutable th : Thread.t option
-  }
+type 'a t = { work_chan : 'a Channel.t } [@@unboxed]
 
 type task = Channel.elt_in_channel
 
-let run (f, t) =
+let run t f =
   let rec loop () =
     match Channel.get t.work_chan with
     | Ok v ->
@@ -18,9 +15,8 @@ let run (f, t) =
   loop ()
 
 let create ~do_ =
-  let t = { work_chan = Channel.create (); th = None } in
-  let th = Thread.create run (do_, t) in
-  t.th <- Some th;
+  let t = { work_chan = Channel.create () } in
+  let _th = Thread.create (fun f -> run t f) do_ in
   t
 
 let add_work t v =
@@ -33,6 +29,4 @@ let cancel = Channel.remove_if_not_consumed
 let complete_tasks_and_stop t =
   match Channel.close t.work_chan with
   | Ok () -> ()
-  | Error `Already_closed ->
-    (* failwith "channel already closed" *)
-    ()
+  | Error `Already_closed -> ()
