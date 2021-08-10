d94ff0f97302b6b277e9262c10940f4c7d2fbb73
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-08-10 10:14:40 +0500

    simplify `Removable_queue`

--- fiber-unix/src/removable_queue.ml
+++ fiber-unix/src/removable_queue.ml
@@ -1,82 +1,13 @@
-open Import
+type 'a node = 'a Doubly_linked.node
 
-type 'a node =
-  { mutable next : 'a node option
-  ; mutable prev : 'a node option
-  ; data : 'a
-  ; mutable queue : 'a t option
-  }
+type 'a t = 'a Doubly_linked.t
 
-and 'a non_empty =
-  { tail : 'a node
-  ; head : 'a node
-  }
+let create = Doubly_linked.create
 
-and 'a queue =
-  | Empty
-  | Non_empty of 'a non_empty
+let is_empty = Doubly_linked.is_empty
 
-and 'a t = 'a queue ref
+let push = Doubly_linked.append
 
-let create () = ref Empty
+let pop = Doubly_linked.detach_head
 
-let is_empty t = !t = Empty
-
-let push (type a) (t : a t) (a : a) =
-  let node prev = { next = None; prev; data = a; queue = Some t } in
-  match !t with
-  | Empty ->
-    let node = node None in
-    t := Non_empty { tail = node; head = node };
-    node
-  | Non_empty ne ->
-    let node = node (Some ne.tail) in
-    assert (ne.tail.next = None);
-    ne.tail.next <- Some node;
-    t := Non_empty { ne with tail = node };
-    node
-
-let pop (type a) (t : a t) : a option =
-  match !t with
-  | Empty -> None
-  | Non_empty ne ->
-    let node = ne.head in
-    if Option.is_none node.next then
-      t := Empty
-    else
-      t := Non_empty { ne with head = Option.value_exn node.next };
-    node.prev <- None;
-    node.next <- None;
-    node.queue <- None;
-    Some node.data
-
-let remove node =
-  match node.queue with
-  | None (* case: node was already removed *) -> ()
-  | Some queue -> (
-    match !queue with
-    | Empty -> assert false
-    | Non_empty curr_q -> (
-      match (node.next, node.prev) with
-      | None, None (* case: queue consists of a single node *) -> queue := Empty
-      | _, _ ->
-        let tail =
-          match node.next with
-          | None (* case: node is the last node in queue *) ->
-            Option.value_exn node.prev
-          | Some next ->
-            next.prev <- node.prev;
-            curr_q.tail
-        in
-        let head =
-          match node.prev with
-          | None (* case: node is the head node in queue *) ->
-            Option.value_exn node.next
-          | Some prev ->
-            prev.next <- node.next;
-            curr_q.head
-        in
-        queue := Non_empty { head; tail };
-        node.prev <- None;
-        node.next <- None;
-        node.queue <- None))
+let remove node = Doubly_linked.detach node |> ignore
