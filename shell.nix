94bf887969ddd92398676f9b57a15fad236b43b6
Rudi Grinberg <me@rgrinberg.com>
2021-10-05 14:25:02 -0600

    [nix] read ocamlformat version from config
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- shell.nix
+++ shell.nix
@@ -1,8 +1,21 @@
 let
   pkgs = (import <nixpkgs> { });
   local = (import ./default.nix { });
-  strings = pkgs.lib.strings;
   inherit (pkgs) stdenv lib;
+  ocamlformat =
+    let
+      ocamlformat_version =
+        let
+          lists = pkgs.lib.lists;
+          strings = pkgs.lib.strings;
+          ocamlformat_config = strings.splitString "\n" (builtins.readFile ./.ocamlformat);
+          prefix = "version=";
+          ocamlformat_version_pred = line: strings.hasPrefix prefix line;
+          version_line = lists.findFirst ocamlformat_version_pred "not_found" ocamlformat_config;
+          version = strings.removePrefix prefix version_line;
+        in
+        builtins.replaceStrings ["."] ["_"] version;
+    in builtins.getAttr ("ocamlformat_" + ocamlformat_version) pkgs;
 in with local;
 
 pkgs.mkShell {
@@ -11,9 +24,8 @@ pkgs.mkShell {
     yarn
     nodejs-14_x
     gnumake
-    ocamlformat_0_19_0
     ocamlPackages.ocaml-lsp
-  ]) ++ (with opam; [
+  ]) ++ [ocamlformat] ++ (with opam; [
     # dev
     cinaps
     menhir
