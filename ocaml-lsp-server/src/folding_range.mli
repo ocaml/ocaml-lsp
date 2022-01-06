4caf06dee9d11f8a555b5a3118720e2ca84280ab
Rudi Grinberg <me@rgrinberg.com>
2022-01-06 09:49:10 -0700

    chore: add folding_range.mli
    ps-id: B7090192-587C-408B-8EED-97B0F6ADFE36

---
+++ ocaml-lsp-server/src/folding_range.mli
@@ -0,0 +1,7 @@
+(** This module is responsible for computing the folding range for OCaml source *)
+
+open Import
+
+(** Compute the folding range *)
+val compute :
+  State.t -> FoldingRangeParams.t -> FoldingRange.t list option Fiber.t
