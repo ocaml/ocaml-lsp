696e77b31054a9f653cbbc1a56dde963078e0407
tatchi <corentin.leruth@gmail.com>
2022-01-19 07:07:18 +0100

    folding: support return type annotation

--- ocaml-lsp-server/test/e2e/__tests__/textDocument-foldingRange.test.ts
+++ ocaml-lsp-server/test/e2e/__tests__/textDocument-foldingRange.test.ts
@@ -321,6 +321,13 @@ describe("textDocument/foldingRange", () => {
           "startCharacter": 0,
           "startLine": 0,
         },
+        Object {
+          "endCharacter": 9,
+          "endLine": 3,
+          "kind": "region",
+          "startCharacter": 2,
+          "startLine": 1,
+        },
       ]
     `);
   });
