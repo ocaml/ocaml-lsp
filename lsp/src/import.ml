123211ec78133a8fd494d91b6a1c73d8a58bdcde
Ulugbek Abdullaev <ulugbekna@gmail.com>
2022-02-08 17:10:47 +0500

    fix: better missing json-field error formatting

--- lsp/src/import.ml
+++ lsp/src/import.ml
@@ -98,7 +98,7 @@ module Json = struct
   let field_exn fields name conv =
     match field fields name conv with
     | Some f -> f
-    | None -> error ("missing field" ^ name) (`Assoc fields)
+    | None -> error ("missing field: " ^ name) (`Assoc fields)
 
   let rec of_dyn (t : Dyn.t) : t =
     match t with
