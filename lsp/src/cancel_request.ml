e4bbe1419ba90a06a7f1be75690230112933c55a
Ulugbek Abdullaev <ulugbekna@gmail.com>
2022-02-08 17:11:26 +0500

    fix: $/cancelRequest params were incorrectly (de)serialized

--- lsp/src/cancel_request.ml
+++ lsp/src/cancel_request.ml
@@ -4,7 +4,7 @@ let meth_ = "$/cancelRequest"
 
 let t_of_yojson json =
   match json with
-  | `Assoc fields -> Json.field_exn fields "params" Jsonrpc.Id.t_of_yojson
+  | `Assoc fields -> Json.field_exn fields "id" Jsonrpc.Id.t_of_yojson
   | _ -> Json.error "invalid id" json
 
-let yojson_of_t id = `Assoc [ ("params", Jsonrpc.Id.yojson_of_t id) ]
+let yojson_of_t id = `Assoc [ ("id", Jsonrpc.Id.yojson_of_t id) ]
