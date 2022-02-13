765652791e167182dbe0f0c781bf4e561b0a0765
Rudi Grinberg <me@rgrinberg.com>
2022-02-12 22:43:17 -0700

    chore: add empty mli
    ps-id: A999978A-0434-4FBC-8E95-64D9259DEC64

--- fiber-test/fiber_test.ml
+++ fiber-test/fiber_test.ml
@@ -1,7 +1,4 @@
 open Stdune
-open Fiber.O
-
-let printf = Printf.printf
 
 let print pp = Format.printf "%a@." Pp.to_fmt pp
 
@@ -12,8 +9,6 @@ module Scheduler : sig
 
   exception Never
 
-  val yield : unit -> unit Fiber.t
-
   val create : unit -> t
 
   val run : t -> 'a Fiber.t -> 'a
@@ -23,12 +18,6 @@ end = struct
   let t_var = Fiber.Var.create ()
 
   let create () = Queue.create ()
-
-  let yield () =
-    let ivar = Fiber.Ivar.create () in
-    let* t = Fiber.Var.get_exn t_var in
-    Queue.push t ivar;
-    Fiber.Ivar.read ivar
 
   exception Never
 
