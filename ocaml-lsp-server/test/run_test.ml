28e5ccf3ceaf9724aecf4efeba4bd665540e2e76
Corentin Leruth <corentin.leruth@gmail.com>
2022-05-25 08:59:32 +0200

    use yarn

--- ocaml-lsp-server/test/run_test.ml
+++ ocaml-lsp-server/test/run_test.ml
@@ -1,12 +1,11 @@
 let _ =
-  let () = Sys.chdir Fpath.(v "ocaml-lsp-server/test/e2e" |> to_string) in
   let sh, flag = if Sys.win32 then ("cmd", "/C") else ("sh", "-c") in
   let cmd =
-    Bos.Cmd.(v sh % flag % Fpath.(v "./node_modules/.bin/jest" |> to_string))
+    Bos.Cmd.(v sh % flag % "yarn --cwd ocaml-lsp-server/test/e2e test")
   in
   let result = Bos.OS.Cmd.(run_out ~err:err_run_out cmd |> to_stdout) in
   match result with
   | Ok _ -> ()
   | Error (`Msg e) ->
-    Printf.eprintf "[Error]: %s" e;
+    Printf.eprintf "[Error]: %s\n" e;
     exit 1
