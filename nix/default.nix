b704362f027454ba21005c521e93e909379eb92d
Rudi Grinberg <me@rgrinberg.com>
2022-06-03 10:52:39 -0500

    chore: update nix build plan
    ps-id: 2E340743-C9DB-4A52-88EF-EE3DFCD0BF57

--- nix/default.nix
+++ nix/default.nix
@@ -2,12 +2,6 @@
 { pkgs, stdenv, fetchFromGitHub, opam2nix }:
 let
   strings = pkgs.lib.strings;
-  ocamlformat = fetchFromGitHub {
-		owner = "ocaml-ppx";
-		repo = "ocamlformat";
-		rev = "93a6b2f46cf31237c413c1d0ac604a8d69676297";
-		sha256 = "sha256-2MFiuLa3pLdT0LYVFYjI4ocNsVcEpeWiyrCX2Aknh/g=";
-  };
   ourSrc = let ignores = pkgs.lib.strings.fileContents ../.gitignore
              + builtins.foldl' (acc: e: acc + "\n" + e) "\n"
                  [ "nix" "shell.nix" "default.nix" ];
@@ -16,8 +10,6 @@ let
     inherit (pkgs.ocaml-ng.ocamlPackages_4_14) ocaml;
     selection = ./opam-selection.nix;
     src = {
-      ocamlformat-rpc-lib = ocamlformat;
-      ocamlformat-rpc = ocamlformat;
       lsp = ourSrc;
       jsonrpc = ourSrc;
       ocaml-lsp-server = ourSrc;
@@ -26,11 +18,10 @@ let
   opam-selection = opam2nix.build args;
   localPackages = let contents = builtins.attrNames (builtins.readDir ../.);
   in builtins.filter (strings.hasSuffix ".opam") contents;
-  resolve = opam2nix.resolve args (localPackages ++
-    ["${ocamlformat}/ocamlformat-rpc-lib.opam" "${ocamlformat}/ocamlformat-rpc.opam"] ++ [
+  resolve = opam2nix.resolve args (localPackages ++ [
     # dev deps
     "cinaps"
-    "menhir=20211128"
+    "menhir"
     "ppx_yojson_conv"
 
     # test deps
