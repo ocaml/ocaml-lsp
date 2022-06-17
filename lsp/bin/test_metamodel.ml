56dae72ba4347603451abdaf8fdebe27cc913222
Rudi Grinberg <me@rgrinberg.com>
2022-06-17 13:49:57 -0500

    feature(lsp): add metamodel parser
    in preparation for 3.17
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: E0AE7554-7060-4327-854C-5BABF6DB7742

---
+++ lsp/bin/test_metamodel.ml
@@ -0,0 +1,8 @@
+let file = Sys.argv.(1)
+
+let () =
+  let read = open_in file in
+  let s = really_input_string read (in_channel_length read) in
+  let json = Yojson.Safe.from_string s in
+  let (_ : Metamodel.t) = Metamodel.t json in
+  close_in read
