896b146af7d88cd078bed53d1623088b9fed6873
Ali Caglayan <alizter@gmail.com>
2022-06-25 01:39:37 +0200

    wip

---
+++ format.nix
@@ -0,0 +1,37 @@
+let
+  pkgs = (import <nixpkgs> { });
+  local = (import ./default.nix { });
+  inherit (pkgs) stdenv lib;
+  ocamlformat =
+    let
+      ocamlformat_version =
+        let
+          lists = pkgs.lib.lists;
+          strings = pkgs.lib.strings;
+          ocamlformat_config = strings.splitString "\n" (builtins.readFile ./.ocamlformat);
+          prefix = "version=";
+          ocamlformat_version_pred = line: strings.hasPrefix prefix line;
+          version_line = lists.findFirst ocamlformat_version_pred "not_found" ocamlformat_config;
+          version = strings.removePrefix prefix version_line;
+        in
+        builtins.replaceStrings ["."] ["_"] version;
+    in builtins.getAttr ("ocamlformat_" + ocamlformat_version) pkgs;
+in with local;
+
+pkgs.mkShell {
+  inputsFrom = [ jsonrpc lsp ocaml-lsp-server ];
+  buildInputs = (with pkgs; [
+    yarn
+    nodejs-14_x
+    gnumake
+  ]) ++ [ocamlformat] ++ (with opam; [
+    # dev
+    cinaps
+    menhir
+    ppx_yojson_conv
+
+    # test
+    ppx_expect
+    ocamlformat-rpc
+  ]);
+}
