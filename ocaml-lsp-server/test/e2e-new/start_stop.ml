c36cab7663355bbb64ead5733722c602acb7b134
Rudi Grinberg <me@rgrinberg.com>
2022-07-02 16:25:18 -0500

    test(ocamllsp): extract e2e-new helpers
    to make it easy to write them in OCaml
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: e2a40a58-1e8b-451f-9637-a8400cad373c

--- ocaml-lsp-server/test/e2e-new/start_stop.ml
+++ ocaml-lsp-server/test/e2e-new/start_stop.ml
@@ -1,41 +1,7 @@
-open Stdune
-open Fiber.O
-
-let _PATH =
-  Bin.parse_path (Option.value ~default:"" @@ Env.get Env.initial "PATH")
-
-let bin = Bin.which "ocamllsp" ~path:_PATH |> Option.value_exn |> Path.to_string
-
-let env = Spawn.Env.of_list [ "OCAMLLSP_TEST=true" ]
-
-module Client = Lsp_fiber.Client
-open Lsp.Types
+open Test.Import
 
 let%expect_test "start/stop" =
-  let stdin_i, stdin_o = Unix.pipe ~cloexec:true () in
-  let stdout_i, stdout_o = Unix.pipe ~cloexec:true () in
-  let pid =
-    Spawn.spawn ~env ~prog:bin ~argv:[ bin ] ~stdin:stdin_i ~stdout:stdout_o ()
-  in
-  Unix.close stdin_i;
-  Unix.close stdout_o;
-  let handler = Client.Handler.make () in
-  let init =
-    let blockity =
-      if Sys.win32 then `Blocking
-      else (
-        Unix.set_nonblock stdout_i;
-        Unix.set_nonblock stdin_o;
-        `Non_blocking true)
-    in
-    let make fd what =
-      let fd = Lev_fiber.Fd.create fd blockity in
-      Lev_fiber.Io.create fd what
-    in
-    let* in_ = make stdout_i Input in
-    let* out = make stdin_o Output in
-    let io = Lsp_fiber.Fiber_io.make in_ out in
-    let client = Client.make handler io () in
+  ( Test.run @@ fun client ->
     let run_client () =
       let capabilities = ClientCapabilities.create () in
       Client.start client (InitializeParams.create ~capabilities ())
@@ -53,12 +19,7 @@ let%expect_test "start/stop" =
       Client.request client Shutdown
     in
     Fiber.fork_and_join_unit run_client (fun () -> run >>> Client.stop client)
-  in
-  let waitpid =
-    let+ (_ : Unix.process_status) = Lev_fiber.waitpid ~pid in
-    ()
-  in
-  Lev_fiber.run (fun () -> Fiber.all_concurrently_unit [ init; waitpid ]);
+  );
   [%expect
     {|
       client: server initialized with:
---
+++ ocaml-lsp-server/test/e2e-new/test.ml
@@ -0,0 +1,56 @@
+module Import = struct
+  include Stdune
+  include Fiber.O
+  module Client = Lsp_fiber.Client
+  include Lsp.Types
+end
+
+open Import
+
+module T : sig
+  val run : (unit Client.t -> unit Fiber.t) -> unit
+end = struct
+  let _PATH =
+    Bin.parse_path (Option.value ~default:"" @@ Env.get Env.initial "PATH")
+
+  let bin =
+    Bin.which "ocamllsp" ~path:_PATH |> Option.value_exn |> Path.to_string
+
+  let env = Spawn.Env.of_list [ "OCAMLLSP_TEST=true" ]
+
+  let run f =
+    let stdin_i, stdin_o = Unix.pipe ~cloexec:true () in
+    let stdout_i, stdout_o = Unix.pipe ~cloexec:true () in
+    let pid =
+      Spawn.spawn ~env ~prog:bin ~argv:[ bin ] ~stdin:stdin_i ~stdout:stdout_o
+        ()
+    in
+    Unix.close stdin_i;
+    Unix.close stdout_o;
+    let handler = Client.Handler.make () in
+    let init =
+      let blockity =
+        if Sys.win32 then `Blocking
+        else (
+          Unix.set_nonblock stdout_i;
+          Unix.set_nonblock stdin_o;
+          `Non_blocking true)
+      in
+      let make fd what =
+        let fd = Lev_fiber.Fd.create fd blockity in
+        Lev_fiber.Io.create fd what
+      in
+      let* in_ = make stdout_i Input in
+      let* out = make stdin_o Output in
+      let io = Lsp_fiber.Fiber_io.make in_ out in
+      let client = Client.make handler io () in
+      f client
+    in
+    let waitpid =
+      let+ (_ : Unix.process_status) = Lev_fiber.waitpid ~pid in
+      ()
+    in
+    Lev_fiber.run (fun () -> Fiber.all_concurrently_unit [ init; waitpid ])
+end
+
+include T
