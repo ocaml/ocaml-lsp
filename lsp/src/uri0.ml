e01ff03f61a5e2d435deba48d15ca8cfadcf2970
Corentin Leruth <corentin.leruth@gmail.com>
2022-07-07 19:59:11 +0200

    refactor(uri): refactor uri impl

--- lsp/src/uri0.ml
+++ lsp/src/uri0.ml
@@ -29,26 +29,24 @@ let of_path path =
   let path = if !Private.win32 then backslash_to_slash path else path in
   Uri_lexer.of_path path
 
+let is_letter = function
+  | 'a' .. 'z' | 'A' .. 'Z' -> true
+  | _ -> false
+
 let to_path { path; authority; scheme } =
   let path =
     let len = String.length path in
     if len = 0 then "/"
     else
       let buff = Buffer.create 64 in
-      (if (not (String.is_empty authority)) && len > 1 && scheme = "file" then (
-       Buffer.add_string buff "//";
-       Buffer.add_string buff authority;
-       Buffer.add_string buff path)
+      (if (not (String.is_empty authority)) && len > 1 && scheme = "file" then
+       Buffer.add_string buff (Printf.sprintf "//%s%s" authority path)
       else if len < 3 then Buffer.add_string buff path
       else
         let c0 = path.[0] in
         let c1 = path.[1] in
         let c2 = path.[2] in
-        if
-          c0 = '/'
-          && ((c1 >= 'A' && c1 <= 'Z') || (c1 >= 'a' && c1 <= 'z'))
-          && c2 = ':'
-        then (
+        if c0 = '/' && is_letter c1 && c2 = ':' then (
           Buffer.add_char buff (Char.lowercase_ascii c1);
           Buffer.add_substring buff path 2 (String.length path - 2))
         else Buffer.add_string buff path);
@@ -65,9 +63,8 @@ let encode ?(allow_slash = false) s =
 let to_string { scheme; authority; path } =
   let buff = Buffer.create 64 in
 
-  if not (String.is_empty scheme) then (
-    Buffer.add_string buff scheme;
-    Buffer.add_char buff ':');
+  if not (String.is_empty scheme) then
+    Buffer.add_string buff (Printf.sprintf "%s:" scheme);
 
   if authority = "file" || scheme = "file" then Buffer.add_string buff "//";
 
@@ -82,20 +79,19 @@ let to_string { scheme; authority; path } =
    let encoded_colon = "%3A" in
    let len = String.length path in
    if len >= 3 && path.[0] = '/' && path.[2] = ':' then (
-     let drive_letter = Char.lowercase_ascii path.[1] in
-     if drive_letter >= 'a' && drive_letter <= 'z' then (
-       Buffer.add_char buff '/';
-       Buffer.add_char buff drive_letter;
-       Buffer.add_string buff encoded_colon;
-       let s = String.sub path ~pos:3 ~len:(len - 3) in
-       Buffer.add_string buff (encode s)))
+     let drive_letter = path.[1] in
+     if is_letter drive_letter then
+       let drive_letter = Char.lowercase_ascii drive_letter in
+       let s = encode (String.sub path ~pos:3 ~len:(len - 3)) in
+       Buffer.add_string buff
+         (Printf.sprintf "/%c%s%s" drive_letter encoded_colon s))
    else if len >= 2 && path.[1] = ':' then (
-     let drive_letter = Char.lowercase_ascii path.[0] in
-     if drive_letter >= 'a' && drive_letter <= 'z' then (
-       Buffer.add_char buff drive_letter;
-       Buffer.add_string buff encoded_colon;
-       let s = String.sub path ~pos:2 ~len:(len - 2) in
-       Buffer.add_string buff (encode s)))
+     let drive_letter = path.[0] in
+     if is_letter drive_letter then
+       let drive_letter = Char.lowercase_ascii drive_letter in
+       let s = encode (String.sub path ~pos:2 ~len:(len - 2)) in
+       Buffer.add_string buff
+         (Printf.sprintf "%c%s%s" drive_letter encoded_colon s))
    else Buffer.add_string buff (encode path));
 
   Buffer.contents buff
