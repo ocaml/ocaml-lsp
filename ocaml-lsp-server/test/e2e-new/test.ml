0a2a8ad96acb19c9b4277be3f2ef6c2a7525dfaa
Rudi Grinberg <me@rgrinberg.com>
2022-07-31 10:00:48 -0500

    test: timeout e2e tests
    ps-id: 9ef4b828-6c3d-4439-9e3f-5795bd3f3728

--- ocaml-lsp-server/test/e2e-new/test.ml
+++ ocaml-lsp-server/test/e2e-new/test.ml
@@ -55,11 +55,29 @@ end = struct
       let client = Client.make handler io () in
       f client
     in
-    let waitpid =
-      let+ (_ : Unix.process_status) = Lev_fiber.waitpid ~pid in
-      ()
+    (* TODO replace the wheel once we can cancel sleep *)
+    let waitpid wheel =
+      let* timeout = Lev_fiber.Timer.Wheel.task wheel in
+      Fiber.finalize ~finally:(fun () -> Lev_fiber.Timer.Wheel.stop wheel)
+      @@ fun () ->
+      let cancelled = ref false in
+      Fiber.fork_and_join_unit
+        (fun () ->
+          let+ timeout = Lev_fiber.Timer.Wheel.await timeout in
+          match timeout with
+          | `Ok ->
+            Unix.kill pid Sys.sigkill;
+            cancelled := true
+          | `Cancelled -> ())
+        (fun () ->
+          let* (_ : Unix.process_status) = Lev_fiber.waitpid ~pid in
+          if !cancelled then Fiber.return ()
+          else Lev_fiber.Timer.Wheel.cancel timeout)
     in
-    Lev_fiber.run (fun () -> Fiber.all_concurrently_unit [ init; waitpid ])
+    Lev_fiber.run (fun () ->
+        let* wheel = Lev_fiber.Timer.Wheel.create ~delay:3.0 in
+        Fiber.all_concurrently_unit
+          [ init; waitpid wheel; Lev_fiber.Timer.Wheel.run wheel ])
     |> Lev_fiber.Error.ok_exn
 end
 
