e67579cb963b511ec33fe6cec41a732ca887e52d
Jack Feser <feser@mit.edu>
2022-08-30 15:13:26 -0400

    tag deprecated warnings

--- ocaml-lsp-server/src/code_actions/diagnostic_util.ml
+++ ocaml-lsp-server/src/code_actions/diagnostic_util.ml
@@ -3,3 +3,6 @@ open Import
 let is_unused_var_warning s =
   String.is_prefix s ~prefix:"Error (warning 26)"
   || String.is_prefix s ~prefix:"Error (warning 27)"
+
+let is_deprecated_warning s =
+  String.is_prefix s ~prefix:"Error (alert deprecated)"
