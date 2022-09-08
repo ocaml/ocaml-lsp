f6839be1386fd6d1f0ab0087076e43e72fa0b97f
Jack Feser <feser@mit.edu>
2022-09-08 15:28:08 -0400

    address comments

--- ocaml-lsp-server/src/code_actions/action_inline.ml
+++ ocaml-lsp-server/src/code_actions/action_inline.ml
@@ -3,8 +3,8 @@ open Import
 let action_title = "Inline"
 
 type inline_task =
-  { ident : Ident.t
-  ; body : Typedtree.expression  (** the expression to inline *)
+  { inlined_var : Ident.t
+  ; inlined_expr : Typedtree.expression  (** the expression to inline *)
   ; context : Typedtree.expression  (** where to perform inlining *)
   }
 
@@ -25,7 +25,7 @@ let find_inline_task pipeline pos =
     (Mpipeline.get_lexing_pos pipeline (Position.logical pos))
     [ browse ]
   |> List.find_map ~f:(function
-       | ( _
+       | ( (_ : Ocaml_typing.Env.t)
          , Browse_raw.Expression
              { exp_desc =
                  Texp_let
@@ -39,35 +39,34 @@ let find_inline_task pipeline pos =
              ; _
              } )
          when contains s.loc pos ->
-         Some { ident = id; body = vb_expr; context = rhs }
+         Some { inlined_var = id; inlined_expr = vb_expr; context = rhs }
        | _ -> None)
 
 let find_parsetree_loc pipeline loc k =
-  let expr_iter (iter : Ocaml_parsing.Ast_iterator.iterator)
-      (expr : Parsetree.expression) =
-    if expr.pexp_loc = loc then k expr
-    else Ocaml_parsing.Ast_iterator.default_iterator.expr iter expr
+  let expr_iter (iter : Ast_iterator.iterator) (expr : Parsetree.expression) =
+    if Loc.compare expr.pexp_loc loc = 0 then k expr
+    else Ast_iterator.default_iterator.expr iter expr
   in
-  let iterator =
-    { Ocaml_parsing.Ast_iterator.default_iterator with expr = expr_iter }
-  in
-  match Mpipeline.reader_parsetree pipeline with
+  let iterator = { Ast_iterator.default_iterator with expr = expr_iter } in
+  match Mpipeline.ppx_parsetree pipeline with
   | `Implementation s -> iterator.structure iterator s
-  | _ -> ()
+  | `Interface _ -> ()
 
 let inlined_text pipeline task =
   let ret = ref None in
-  find_parsetree_loc pipeline task.body.exp_loc (fun expr ->
+  find_parsetree_loc pipeline task.inlined_expr.exp_loc (fun expr ->
       ret :=
         Some (Format.asprintf "(%a)" Ocaml_parsing.Pprintast.expression expr));
   Option.value_exn !ret
 
 (** Iterator over the text edits performed by the inlining task. *)
-let iter_inline_edits pipeline task k =
+let inline_edits pipeline task =
   let newText = inlined_text pipeline task in
   let make_edit newText loc =
     TextEdit.create ~newText ~range:(Range.of_loc loc)
   in
+  let edits = Queue.create () in
+  let insert_edit newText loc = Queue.push edits (make_edit newText loc) in
 
   let expr_iter (iter : Ocaml_typing.Tast_iterator.iterator)
       (expr : Typedtree.expression) =
@@ -80,40 +79,42 @@ let iter_inline_edits pipeline task k =
           (* handle the labeled argument shorthand `f ~x` when inlining `x` *)
           | ( Asttypes.Labelled name
             , Some { exp_desc = Texp_ident (Pident id, { loc; _ }, _); _ } )
-            when Ident.same task.ident id ->
+            when Ident.same task.inlined_var id ->
             let newText = sprintf "%s:%s" name newText in
-            k (make_edit newText loc)
+            insert_edit newText loc
           | _, m_expr -> Option.iter m_expr ~f:(iter.expr iter))
-    | Texp_ident (Pident id, { loc; _ }, _) when Ident.same task.ident id ->
-      k (make_edit newText loc)
+    | Texp_ident (Pident id, { loc; _ }, _) when Ident.same task.inlined_var id
+      -> insert_edit newText loc
     | _ -> Ocaml_typing.Tast_iterator.default_iterator.expr iter expr
   in
   let iterator =
     { Ocaml_typing.Tast_iterator.default_iterator with expr = expr_iter }
   in
-  iterator.expr iterator task.context
+  iterator.expr iterator task.context;
+  Queue.to_list edits
 
 let code_action doc (params : CodeActionParams.t) =
-  Document.with_pipeline_exn doc (fun pipeline ->
-      let m_inline_task = find_inline_task pipeline params.range.start in
-      Option.map m_inline_task ~f:(fun task ->
-          let edits = Queue.create () in
-          iter_inline_edits pipeline task (Queue.push edits);
-
-          let edit =
-            let version = Document.version doc in
-            let textDocument =
-              OptionalVersionedTextDocumentIdentifier.create
-                ~uri:params.textDocument.uri ~version ()
-            in
-            let edit =
-              TextDocumentEdit.create ~textDocument
-                ~edits:
-                  (Queue.to_list edits |> List.map ~f:(fun e -> `TextEdit e))
-            in
-            WorkspaceEdit.create ~documentChanges:[ `TextDocumentEdit edit ] ()
-          in
-          CodeAction.create ~title:action_title
-            ~kind:CodeActionKind.RefactorInline ~edit ~isPreferred:false ()))
+  let open Fiber.O in
+  let* m_edits =
+    Document.with_pipeline_exn doc (fun pipeline ->
+        find_inline_task pipeline params.range.start
+        |> Option.map ~f:(inline_edits pipeline))
+  in
+  Option.map m_edits ~f:(fun edits ->
+      let edit =
+        let version = Document.version doc in
+        let textDocument =
+          OptionalVersionedTextDocumentIdentifier.create
+            ~uri:params.textDocument.uri ~version ()
+        in
+        let edit =
+          TextDocumentEdit.create ~textDocument
+            ~edits:(List.map edits ~f:(fun e -> `TextEdit e))
+        in
+        WorkspaceEdit.create ~documentChanges:[ `TextDocumentEdit edit ] ()
+      in
+      CodeAction.create ~title:action_title ~kind:CodeActionKind.RefactorInline
+        ~edit ~isPreferred:false ())
+  |> Fiber.return
 
 let t = { Code_action.kind = RefactorInline; run = code_action }
