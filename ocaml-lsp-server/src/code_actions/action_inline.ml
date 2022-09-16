2fe33565da1e03d9405dc120887a41e83b3e3578
Jack Feser <feser@mit.edu>
2022-09-15 20:15:54 -0400

    perform beta reduction after inlining function

--- ocaml-lsp-server/src/code_actions/action_inline.ml
+++ ocaml-lsp-server/src/code_actions/action_inline.ml
@@ -112,6 +112,93 @@ let strip_attribute attr_name expr =
   let mapper = { M.default_mapper with expr = expr_map } in
   mapper.expr mapper expr
 
+(** [uses expr path] returns the number of uses of [path] in [expr]. *)
+let uses expr path =
+  let module I = Ocaml_typing.Tast_iterator in
+  let count = ref 0 in
+  let expr_iter (iter : I.iterator) (expr : Typedtree.expression) =
+    match expr.exp_desc with
+    | Texp_ident (path', _, _) when Path.same path path' -> incr count
+    | _ -> I.default_iterator.expr iter expr
+  in
+  let iterator = { I.default_iterator with expr = expr_iter } in
+  iterator.expr iterator expr;
+  !count
+
+let subst task =
+  let module M = Ocaml_typing.Tast_mapper in
+  let expr_map (map : M.mapper) (expr : Typedtree.expression) =
+    match expr.exp_desc with
+    | Texp_ident (Pident id, _, _) when Ident.same task.inlined_var id ->
+      task.inlined_expr
+    | _ -> M.default.expr map expr
+  in
+  let mapper = { M.default with expr = expr_map } in
+  mapper.expr mapper task.context
+
+(** Rough check for pure expressions. Identifiers and constants are pure. *)
+let is_pure (expr : Typedtree.expression) =
+  match expr.exp_desc with
+  | Texp_ident _ | Texp_constant _ -> true
+  | _ -> false
+
+let rec beta_reduce (app : Typedtree.expression) =
+  let untype_expression = Ocaml_typing.Untypeast.untype_expression in
+  match app.exp_desc with
+  | Texp_apply
+      ( { exp_desc =
+            Texp_function
+              { arg_label = Nolabel
+              ; cases =
+                  [ { c_lhs = { pat_desc = pat; _ }
+                    ; c_guard = None
+                    ; c_rhs = body
+                    }
+                  ]
+              ; _
+              }
+        ; _
+        }
+      , (Nolabel, Some arg) :: args' ) -> (
+    let body =
+      if List.is_empty args' then body
+      else { app with exp_desc = Texp_apply (body, args') }
+    in
+    match pat with
+    | Tpat_any -> beta_reduce body
+    | Tpat_var (param, _) ->
+      let n_uses = uses body (Path.Pident param) in
+      Printf.eprintf "uses: %d\n%!" n_uses;
+      Out_channel.flush stderr;
+      if n_uses = 0 then Ocaml_typing.Untypeast.untype_expression body
+      else if n_uses = 1 || is_pure arg then
+        beta_reduce
+          (subst { inlined_var = param; inlined_expr = arg; context = body })
+      else
+        (* if the parameter is used multiple times in the body, introduce a let
+           binding so that the parameter is evaluated only once *)
+        let module H = Ocaml_parsing.Ast_helper in
+        let body = untype_expression body in
+        let arg = untype_expression arg in
+        H.Exp.let_
+          Nonrecursive
+          [ H.Vb.mk
+              (H.Pat.var { txt = Ident.name param; loc = !H.default_loc })
+              arg
+          ]
+          body
+    | _ -> untype_expression app)
+  | _ -> untype_expression app
+
+module Test = struct
+  let z =
+    let k = 1 in
+    let f y = y + k in
+    let k = 2 in
+    let y = 2 in
+    f y
+end
+
 let inlined_text pipeline task =
   let open Option.O in
   let+ expr = find_parsetree_loc pipeline task.inlined_expr.exp_loc in
@@ -137,6 +224,7 @@ let inline_edits pipeline task =
       false
   in
 
+  (* inlining into an argument context has some special cases *)
   let arg_iter env (iter : I.iterator) (label : Asttypes.arg_label)
       (m_arg_expr : Typedtree.expression option) =
     match (label, m_arg_expr) with
@@ -159,11 +247,27 @@ let inline_edits pipeline task =
     | _, m_expr -> Option.iter m_expr ~f:(iter.expr iter)
   in
 
+  let apply_iter env (iter : I.iterator) (func : Typedtree.expression) args =
+    iter.expr iter func;
+    List.iter args ~f:(fun (l, e) -> arg_iter env iter l e)
+  in
+
   let expr_iter (iter : I.iterator) (expr : Typedtree.expression) =
     match expr.exp_desc with
-    | Texp_apply (func, args) ->
-      iter.expr iter func;
-      List.iter args ~f:(fun (l, e) -> arg_iter expr.exp_env iter l e)
+    (* when inlining into an application context, attempt to beta reduce the
+       result *)
+    | Texp_apply ({ exp_desc = Texp_ident (Pident id, _, _); _ }, args)
+      when Ident.same task.inlined_var id && not_shadowed expr.exp_env ->
+      let reduced_expr =
+        beta_reduce
+          { expr with exp_desc = Texp_apply (task.inlined_expr, args) }
+      in
+      let newText =
+        Format.asprintf "(%a)" Pprintast.expression
+        @@ strip_attribute "merlin.loc" reduced_expr
+      in
+      insert_edit newText expr.exp_loc
+    | Texp_apply (func, args) -> apply_iter expr.exp_env iter func args
     | Texp_ident (Pident id, { loc; _ }, _)
       when Ident.same task.inlined_var id && not_shadowed expr.exp_env ->
       insert_edit newText loc
