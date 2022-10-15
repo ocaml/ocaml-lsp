7c0c47d079fb6cfc28236dbb7b0509e35ed4b1fe
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2022-10-15 14:45:37 +0200

    Portable yarn path

--- ocaml-lsp-server/test/run_test_e2e.ml
+++ ocaml-lsp-server/test/run_test_e2e.ml
@@ -1,7 +1,10 @@
 let () =
+  let yarn =
+    [ "ocaml-lsp-server"; "test"; "e2e"; "node_modules"; "yarn"; "bin"; "yarn" ]
+  in
   let cmd =
     Filename.quote_command
-      "ocaml-lsp-server/test/e2e/node_modules/yarn/bin/yarn"
+      (String.concat Filename.dir_sep yarn)
       [ "--cwd"; "ocaml-lsp-server/test/e2e"; "test" ]
   in
   Sys.command cmd |> exit
