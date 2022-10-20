664a1a9127c487c220d6a0bc29c3cc40ddde8d03
Rudi Grinberg <me@rgrinberg.com>
2022-10-20 16:01:40 -0600

    refactor: style tweaks in document_symbol (#885)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 0deb2709-dd55-47c5-89b8-128016703f68

--- ocaml-lsp-server/src/document_symbol.ml
+++ ocaml-lsp-server/src/document_symbol.ml
@@ -13,28 +13,26 @@ let outline_kind kind : SymbolKind.t =
   | `Class -> Class
   | `Method -> Method
 
-let range item = Range.of_loc item.Query_protocol.location
-
-let rec symbol item =
-  let children = List.map item.Query_protocol.children ~f:symbol in
-  let range : Range.t = range item in
+let rec symbol (item : Query_protocol.item) =
+  let children = List.map item.children ~f:symbol in
+  let range = Range.of_loc item.location in
   let kind = outline_kind item.outline_kind in
   DocumentSymbol.create
-    ~name:item.Query_protocol.outline_name
+    ~name:item.outline_name
     ~kind
-    ?detail:item.Query_protocol.outline_type
+    ?detail:item.outline_type
     ~deprecated:item.deprecated
     ~range
     ~selectionRange:range
     ~children
     ()
 
-let rec symbol_info ?containerName uri item =
-  let location = { Location.uri; range = range item } in
+let rec symbol_info ?containerName uri (item : Query_protocol.item) =
   let info =
     let kind = outline_kind item.outline_kind in
+    let location = { Location.uri; range = Range.of_loc item.location } in
     SymbolInformation.create
-      ~name:item.Query_protocol.outline_name
+      ~name:item.outline_name
       ~kind
       ~deprecated:false
       ~location
@@ -50,23 +48,16 @@ let symbols_of_outline uri outline =
   List.concat_map ~f:(symbol_info uri) outline
 
 let run (client_capabilities : ClientCapabilities.t) doc uri =
-  let command = Query_protocol.Outline in
-  let+ outline = Document.dispatch_exn doc command in
-  let symbols =
-    let hierarchicalDocumentSymbolSupport =
-      let open Option.O in
-      Option.value
-        (let* textDocument = client_capabilities.textDocument in
-         let* ds = textDocument.documentSymbol in
-         ds.hierarchicalDocumentSymbolSupport)
-        ~default:false
-    in
-    match hierarchicalDocumentSymbolSupport with
-    | true ->
-      let symbols = List.map outline ~f:symbol in
-      `DocumentSymbol symbols
-    | false ->
-      let symbols = symbols_of_outline uri outline in
-      `SymbolInformation symbols
-  in
-  symbols
+  let+ outline = Document.dispatch_exn doc Outline in
+  match
+    Option.value
+      ~default:false
+      (let open Option.O in
+      let* textDocument = client_capabilities.textDocument in
+      let* ds = textDocument.documentSymbol in
+      ds.hierarchicalDocumentSymbolSupport)
+  with
+  | true ->
+    let symbols = List.map outline ~f:symbol in
+    `DocumentSymbol symbols
+  | false -> `SymbolInformation (symbols_of_outline uri outline)
