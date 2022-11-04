335453778aa060a2f6ecb80df97239565b39a621
Louis Roché (Ahrefs) <louis.roche@ahrefs.com>
2022-11-04 03:50:43 +0000

    document hoverExtended state

--- ocaml-lsp-server/src/state.mli
+++ ocaml-lsp-server/src/state.mli
@@ -9,7 +9,13 @@ type init =
       ; exp_client_caps : Client.Experimental_capabilities.t
       }
 
-type hover_extended = { mutable history : (Uri.t * Position.t * int) option }
+(** State specific to the hoverExtended request. *)
+type hover_extended =
+  { mutable history : (Uri.t * Position.t * int) option
+        (** File, position, and verbosity level of the last call to
+            hoverExtended. This value is used to pick a verbosity level when it
+            is not specific by the client. *)
+  }
 
 type t =
   { store : Document_store.t
