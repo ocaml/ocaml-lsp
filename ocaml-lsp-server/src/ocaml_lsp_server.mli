1a16dbd27eebe8b5e3076445a5bcb08ef0d706ec
Rudi Grinberg <me@rgrinberg.com>
2022-12-08 13:07:11 -0600

    test: edit diffing (#945)

--- ocaml-lsp-server/src/ocaml_lsp_server.mli
+++ ocaml-lsp-server/src/ocaml_lsp_server.mli
@@ -2,3 +2,4 @@ val run : read_dot_merlin:bool -> unit -> unit
 
 module Diagnostics = Diagnostics
 module Version = Version
+module Diff = Diff
--- ocaml-lsp-server/test/ocaml_lsp_tests.ml
+++ ocaml-lsp-server/test/ocaml_lsp_tests.ml
@@ -17,3 +17,75 @@ let%expect_test "eat_message tests" =
   [%expect {| [PASS] |}];
   test "foo bar" "foo Bar" false;
   [%expect {| [PASS] |}]
+
+let%test_module "diff" =
+  (module struct
+    let test ~from ~to_ =
+      let edits = Ocaml_lsp_server.Diff.edit ~from ~to_ in
+      let json = `List (List.map Lsp.Types.TextEdit.yojson_of_t edits) in
+      Yojson.Safe.pretty_to_string ~std:false json |> print_endline
+
+    let%expect_test "empty strings" =
+      test ~from:"" ~to_:"";
+      [%expect {| [] |}]
+
+    let%expect_test "from empty" =
+      test ~from:"" ~to_:"foobar";
+      [%expect
+        {|
+        [
+          {
+            "newText": "foobar\n",
+            "range": {
+              "end": { "character": 0, "line": 0 },
+              "start": { "character": 0, "line": 0 }
+            }
+          }
+        ] |}]
+
+    let%expect_test "to empty" =
+      test ~from:"foobar" ~to_:"";
+      [%expect
+        {|
+        [
+          {
+            "newText": "",
+            "range": {
+              "end": { "character": 0, "line": 1 },
+              "start": { "character": 0, "line": 0 }
+            }
+          }
+        ] |}]
+
+    let%expect_test "no change" =
+      test ~from:"foobar" ~to_:"foobar";
+      [%expect {| [] |}]
+
+    let%expect_test "multiline" =
+      test ~from:"foo" ~to_:"baz\nbar\nxx\n";
+      [%expect
+        {|
+        [
+          {
+            "newText": "baz\nbar\nxx\n",
+            "range": {
+              "end": { "character": 0, "line": 1 },
+              "start": { "character": 0, "line": 0 }
+            }
+          }
+        ] |}]
+
+    let%expect_test "change a character" =
+      test ~from:"xxx y xx" ~to_:"xxx z xx";
+      [%expect
+        {|
+        [
+          {
+            "newText": "xxx z xx\n",
+            "range": {
+              "end": { "character": 0, "line": 1 },
+              "start": { "character": 0, "line": 0 }
+            }
+          }
+        ] |}]
+  end)
