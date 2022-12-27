bc32fce7c0d7cc99a6aa2a11801dd018c09272f1
Rudi Grinberg <me@rgrinberg.com>
2022-12-27 08:10:13 -0600

    refactor(ocamllsp): move signature help (#980)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/ocaml_lsp_server.ml
+++ ocaml-lsp-server/src/ocaml_lsp_server.ml
@@ -387,92 +387,6 @@ let location_of_merlin_loc uri : _ -> (_, string) result = function
              let locs = [ { Location.uri; range } ] in
              `Location locs))
 
-let format_doc ~markdown ~doc =
-  `MarkupContent
-    (if markdown then
-     let value =
-       match Doc_to_md.translate doc with
-       | Raw d -> sprintf "(** %s *)" d
-       | Markdown d -> d
-     in
-     { MarkupContent.value; kind = MarkupKind.Markdown }
-    else { MarkupContent.value = doc; kind = MarkupKind.PlainText })
-
-let signature_help (state : State.t)
-    { SignatureHelpParams.textDocument = { uri }; position; _ } =
-  let doc =
-    let store = state.store in
-    Document_store.get store uri
-  in
-  let pos = Position.logical position in
-  let prefix =
-    (* The value of [short_path] doesn't make a difference to the final result
-       because labels cannot include dots. However, a true value is slightly
-       faster for getting the prefix. *)
-    Compl.prefix_of_position (Document.source doc) pos ~short_path:true
-  in
-  (* TODO use merlin resources efficiently and do everything in 1 thread *)
-  match Document.kind doc with
-  | `Other ->
-    let help = SignatureHelp.create ~signatures:[] () in
-    Fiber.return help
-  | `Merlin merlin -> (
-    let* application_signature =
-      Document.Merlin.with_pipeline_exn merlin (fun pipeline ->
-          let typer = Mpipeline.typer_result pipeline in
-          let pos = Mpipeline.get_lexing_pos pipeline pos in
-          let node = Mtyper.node_at typer pos in
-          Signature_help.application_signature node ~prefix)
-    in
-    match application_signature with
-    | None ->
-      let help = SignatureHelp.create ~signatures:[] () in
-      Fiber.return help
-    | Some application_signature ->
-      let prefix =
-        let fun_name =
-          Option.value ~default:"_" application_signature.function_name
-        in
-        sprintf "%s : " fun_name
-      in
-      let offset = String.length prefix in
-      let+ doc =
-        Document.Merlin.doc_comment
-          merlin
-          application_signature.function_position
-      in
-      let info =
-        let parameters =
-          List.map
-            application_signature.parameters
-            ~f:(fun (p : Signature_help.parameter_info) ->
-              let label =
-                `Offset (offset + p.param_start, offset + p.param_end)
-              in
-              ParameterInformation.create ~label ())
-        in
-        let documentation =
-          let open Option.O in
-          let+ doc in
-          let markdown =
-            ClientCapabilities.markdown_support
-              (State.client_capabilities state)
-              ~field:(fun td ->
-                let* sh = td.signatureHelp in
-                let+ si = sh.signatureInformation in
-                si.documentationFormat)
-          in
-          format_doc ~markdown ~doc
-        in
-        let label = prefix ^ application_signature.signature in
-        SignatureInformation.create ~label ?documentation ~parameters ()
-      in
-      SignatureHelp.create
-        ~signatures:[ info ]
-        ~activeSignature:0
-        ?activeParameter:application_signature.active_param
-        ())
-
 let text_document_lens (state : State.t)
     { CodeLensParams.textDocument = { uri }; _ } =
   let store = state.store in
@@ -835,7 +749,7 @@ let on_request :
       ()
   | TextDocumentRename req -> later Rename.rename req
   | TextDocumentFoldingRange req -> later Folding_range.compute req
-  | SignatureHelp req -> later signature_help req
+  | SignatureHelp req -> later Signature_help.run req
   | TextDocumentLinkResolve l -> now l
   | TextDocumentLink _ -> now None
   | WillSaveWaitUntilTextDocument _ -> now None
--- ocaml-lsp-server/src/signature_help.mli
+++ ocaml-lsp-server/src/signature_help.mli
@@ -1,23 +1,3 @@
-open Merlin_kernel
-open Ocaml_parsing
-open Ocaml_typing
+open Import
 
-type parameter_info =
-  { label : Asttypes.arg_label
-  ; param_start : int
-  ; param_end : int
-  ; argument : Typedtree.expression option
-  }
-
-type application_signature =
-  { function_name : string option
-  ; function_position : Msource.position
-  ; signature : string
-  ; parameters : parameter_info list
-  ; active_param : int option
-  }
-
-val application_signature :
-     prefix:string
-  -> ('a * Merlin_specific.Browse_raw.node) list
-  -> application_signature option
+val run : State.t -> SignatureHelpParams.t -> SignatureHelp.t Fiber.t
