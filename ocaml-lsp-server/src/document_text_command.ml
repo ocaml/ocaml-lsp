a78ad1279724373608b00028724ba8ca10805140
Rudi Grinberg <me@rgrinberg.com>
2023-01-10 15:02:47 -0600

    feature: add command to dump a document
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 2b189bd8-65c5-4d1d-bc0f-a440f45dfe88 -->

---
+++ ocaml-lsp-server/src/document_text_command.ml
@@ -0,0 +1,28 @@
+open Import
+open Fiber.O
+
+let command_name = "ocamllsp/show-document-text"
+
+let command_run server store args =
+  let uri =
+    match args with
+    | Some [ `String arg ] -> arg
+    | _ -> assert false
+  in
+  let doc =
+    Document_store.get store (Uri.t_of_yojson (`String uri)) |> Document.text
+  in
+  let uri, chan =
+    Filename.open_temp_file
+      (sprintf "ocamllsp-document.%d" (Unix.getpid ()))
+      (Filename.extension uri)
+  in
+  output_string chan doc;
+  close_out_noerr chan;
+  let req =
+    let uri = Uri.of_path uri in
+    Server_request.ShowDocumentRequest
+      (ShowDocumentParams.create ~uri ~takeFocus:true ())
+  in
+  let+ { ShowDocumentResult.success = _ } = Server.request server req in
+  ()
---
+++ ocaml-lsp-server/src/document_text_command.mli
@@ -0,0 +1,6 @@
+open Import
+
+val command_name : string
+
+val command_run :
+  _ Server.t -> Document_store.t -> Json.t list option -> unit Fiber.t
