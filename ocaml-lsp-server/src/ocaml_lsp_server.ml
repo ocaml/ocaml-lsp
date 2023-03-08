4434bcb588dbd602c89535106af340e9b95000f2
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2023-03-08 17:43:22 +0100

    Handle codeActionLiteralSupport capability

--- ocaml-lsp-server/src/ocaml_lsp_server.ml
+++ ocaml-lsp-server/src/ocaml_lsp_server.ml
@@ -30,21 +30,24 @@ let view_metrics server =
 let initialize_info (client_capabilities : ClientCapabilities.t) :
     InitializeResult.t =
   let codeActionProvider =
-    let codeActionKinds =
-      Action_inferred_intf.kind :: Action_destruct.kind
-      :: List.map
-           ~f:(fun (c : Code_action.t) -> c.kind)
-           [ Action_type_annotate.t
-           ; Action_remove_type_annotation.t
-           ; Action_construct.t
-           ; Action_refactor_open.unqualify
-           ; Action_refactor_open.qualify
-           ; Action_add_rec.t
-           ; Action_inline.t
-           ]
-      |> List.sort_uniq ~compare:Poly.compare
-    in
-    `CodeActionOptions (CodeActionOptions.create ~codeActionKinds ())
+    match client_capabilities.textDocument with
+    | Some { codeAction = Some { codeActionLiteralSupport = Some _; _ }; _ } ->
+      let codeActionKinds =
+        Action_inferred_intf.kind :: Action_destruct.kind
+        :: List.map
+             ~f:(fun (c : Code_action.t) -> c.kind)
+             [ Action_type_annotate.t
+             ; Action_remove_type_annotation.t
+             ; Action_construct.t
+             ; Action_refactor_open.unqualify
+             ; Action_refactor_open.qualify
+             ; Action_add_rec.t
+             ; Action_inline.t
+             ]
+        |> List.sort_uniq ~compare:Poly.compare
+      in
+      `CodeActionOptions (CodeActionOptions.create ~codeActionKinds ())
+    | _ -> `Bool true
   in
   let textDocumentSync =
     `TextDocumentSyncOptions
