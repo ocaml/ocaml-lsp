5f645f8d6240f592bc87f89629c112fd6e41b19b
Jérôme Boillot <me@jerome-boillot.com>
2023-03-10 16:27:25 +0100

    fix: use preprocessed source to highlight long identifiers

--- ocaml-lsp-server/src/semantic_highlighting.ml
+++ ocaml-lsp-server/src/semantic_highlighting.ml
@@ -860,7 +860,7 @@ let compute_tokens doc =
     Document.Merlin.with_pipeline_exn
       ~name:"semantic highlighting"
       doc
-      (fun p -> (Mpipeline.reader_parsetree p, Mpipeline.raw_source p))
+      (fun p -> (Mpipeline.reader_parsetree p, Mpipeline.input_source p))
   in
   let module Fold = Parsetree_fold (struct
     let source = Msource.text source
