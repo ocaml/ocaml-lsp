cbaa820932050455e2241f2ce4574bddac22f1d5
faldor20 <eli.jambu@yahoo.com>
2023-10-10 02:18:55 +1000

    Added Long file for benchmarking and removed unused benchmarks

--- ocaml-lsp-server/bench/ocaml_lsp_bench.ml
+++ ocaml-lsp-server/bench/ocaml_lsp_bench.ml
@@ -1,57 +1,30 @@
-open Core
-open Core_bench
 open Ocaml_lsp_server
 
-let () =
-  let document =
-    "let mem = ListLabels.mem\n\nlet _ = mem ~se" |> Merlin_kernel.Msource.make
-  in
-  let long_document_text =
-    "pam .tsiL se~\n\
-    \    arosietnaorisetnoarisent\n\
-    \    arsotienarositen\n\
-    \    arsotinarsotienarst\n\
-    \    aoris noarisetnaoiresnt aorisent aoierns\n\
-    \     ast. rienrst .rst \n\
-    \    let b a= 5234 n oienar. rsoien . iri i... r\n\
-    \    !@#&984@#$ <><|||>>\n\
-    \    let mem = ListLabels.mem\n\n\
-     let _ = mem ~se"
-  in
-  let long_document = long_document_text |> Merlin_kernel.Msource.make in
 
+
+open Core
+open Core_bench
+
+let () =
+  let open Documents in
+  let long_document = long_document_text |> Merlin_kernel.Msource.make in
   let position = `Logical (3, 15) in
+  let long_position = `Logical (92, 41) in
   Command_unix.run
     (Bench.make_command
-       [ Bench.Test.create ~name:"regex" (fun _ ->
+       [ Bench.Test.create ~name:"get_prefix" (fun _ ->
              Testing.Compl.prefix_of_position
                ~short_path:false
                document
                position
              |> ignore)
-       ; Bench.Test.create ~name:"regex_long" (fun _ ->
+       ; Bench.Test.create ~name:"get_prefix_long" (fun _ ->
              Testing.Compl.prefix_of_position
                ~short_path:false
                long_document
-               position
+               long_position
              |> ignore)
-       ; Bench.Test.create ~name:"regex_only" (fun _ ->
-             Testing.Prefix_parser.try_parse_with_regex
-               (document |> Merlin_kernel.Msource.text)
-             |> ignore)
-       ; Bench.Test.create ~name:"regex_only_long" (fun _ ->
-             Testing.Prefix_parser.try_parse_with_regex long_document_text
-             |> ignore)
-       ; Bench.Test.create ~name:"old" (fun _ ->
-             Testing.Compl.prefix_of_position_old
-               ~short_path:false
-               document
-               position
-             |> ignore)
-       ; Bench.Test.create ~name:"old_long" (fun _ ->
-             Testing.Compl.prefix_of_position_old
-               ~short_path:false
-               document
-               position
+       ; Bench.Test.create ~name:"get_offset_long" (fun _ ->
+             Merlin_kernel.Msource.get_offset long_document long_position
              |> ignore)
        ])
