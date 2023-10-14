c4249a80a3e304288064e90c590ab2e56062ea4e
faldor20 <eli.jambu@yahoo.com>
2023-10-15 07:20:23 +1000

    fix bad interface

--- ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
+++ ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
@@ -1,7 +1,9 @@
+open Test.Import
+
 (**Opens a document with the language server. This must be done before trying to
    access it*)
 val openDocument :
-     client:'a Ocaml_lsp_e2e.Test.Import.Client.t
+     client:'a Client.t
   -> uri:Lsp__Types.DocumentUri.t
   -> source:string
   -> unit Fiber.t
@@ -9,11 +11,11 @@ val openDocument :
 (**Performs the request you return from the makeRequest function and then gives
    it the the handler function you provide*)
 val iter_LspResponse :
-     ?prep:(unit Ocaml_lsp_e2e.Test.Import.Client.t -> unit Fiber.t)
+     ?prep:(unit Client.t -> unit Fiber.t)
   -> ?path:string
   -> makeRequest:
-       (   Ocaml_lsp_e2e.Test.Import.TextDocumentIdentifier.t
-        -> 'a Ocaml_lsp_e2e.Test.Import.Client.out_request)
+       (   TextDocumentIdentifier.t
+        -> 'a Client.out_request)
   -> source:string
   -> ('a -> unit)
   -> unit
