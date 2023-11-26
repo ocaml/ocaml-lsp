6cc4a7bd5440e1b3179129ac4638d710f1aef9ea
Rudi Grinberg <me@rgrinberg.com>
2023-11-26 12:02:08 -0600

    _
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
+++ ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
@@ -3,10 +3,7 @@ open Test.Import
 (** Opens a document with the language server. This must be done before trying
     to access it *)
 val openDocument :
-     client:'a Client.t
-  -> uri:DocumentUri.t
-  -> source:string
-  -> unit Fiber.t
+  client:'a Client.t -> uri:DocumentUri.t -> source:string -> unit Fiber.t
 
 (** Performs the request you return from the makeRequest function and then gives
     it the the handler function you provide *)
