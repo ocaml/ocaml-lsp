b659cefce90766b571afbcac1c2496c5699ac853
Rudi Grinberg <me@rgrinberg.com>
2023-11-26 11:44:25 -0600

    _
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
+++ ocaml-lsp-server/test/e2e-new/lsp_helpers.mli
@@ -1,15 +1,15 @@
 open Test.Import
 
-(**Opens a document with the language server. This must be done before trying to
-   access it*)
+(** Opens a document with the language server. This must be done before trying
+    to access it *)
 val openDocument :
      client:'a Client.t
   -> uri:Lsp__Types.DocumentUri.t
   -> source:string
   -> unit Fiber.t
 
-(**Performs the request you return from the makeRequest function and then gives
-   it the the handler function you provide*)
+(** Performs the request you return from the makeRequest function and then
+    gives it the the handler function you provide *)
 val iter_LspResponse :
      ?prep:(unit Client.t -> unit Fiber.t)
   -> ?path:string
