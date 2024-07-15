cc8ac40f9d8ea09abd877e6025820c8f6ea4340f
PizieDust <playersrebirth@gmail.com>
2024-07-15 13:19:51 +0100

    remove unnessary methods

--- ocaml-lsp-server/src/custom_requests/req_documentation.mli
+++ ocaml-lsp-server/src/custom_requests/req_documentation.mli
@@ -1,35 +1,13 @@
+open Import
+
 val meth : string
 
 val capability : string * [> `Bool of bool ]
 
 module GetDocClientCapabilities : sig
-  type t = { contentFormat : Import.MarkupKind.t list option }
+  type t = { contentFormat : MarkupKind.t list }
 
-  val yojson_of_t :
-    t -> [> `Assoc of (string * [> `List of Yojson.Safe.t list ]) list ]
-
-  val t_of_yojson : Yojson.Safe.t -> t
-end
-
-module GetDocParams : sig
-  type t =
-    { text_document : Import.TextDocumentIdentifier.t
-    ; position : Position.t
-    ; identifier : string option
-    ; contentFormat : Import.MarkupKind.t option
-    }
-
-  val yojson_of_t : t -> [> `Assoc of (string * Yojson.Safe.t) list ]
-
-  val t_of_yojson : Yojson.Safe.t -> t
-end
-
-module GetDoc : sig
-  type t = { doc : Import.MarkupContent.t }
-
-  val yojson_of_t : t -> [> `Assoc of (string * Yojson.Safe.t) list ]
-
-  val t_of_yojson : Yojson.Safe.t -> t
+  val yojson_of_t : t -> [> `Assoc of [> `List of Json.t list ] ]
 end
 
 val on_request :
