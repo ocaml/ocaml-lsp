e5a83f8675a588ab1d21d34a3e57810d90b47974
Ulysse Gérard <thevoodoos@gmail.com>
2024-10-03 15:39:53 +0200

    Add more tests
    Co-authored-by: PizieDust <playersrebirth@gmail.com>

--- ocaml-lsp-server/test/e2e/__tests__/textDocument-selectionRange.test.ts
+++ ocaml-lsp-server/test/e2e/__tests__/textDocument-selectionRange.test.ts
@@ -148,4 +148,550 @@ Array [
 ]
 `);
   });
+
+  it("returns a selection range for more complex documents", async () => {
+    openDocument(outdent`
+      type _ typ =
+        | TInt : int typ
+        | TBool : bool typ
+      module M = struct
+        type t
+        let f (_ : _ typ) = ()
+      end
+        `);
+
+    let result = await selectionRange([Types.Position.create(5, 23)]);
+    expect(result).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "parent": Object {
+      "parent": Object {
+        "parent": Object {
+          "parent": Object {
+            "parent": Object {
+              "parent": Object {
+                "parent": Object {
+                  "parent": Object {
+                    "range": Object {
+                      "end": Object {
+                        "character": 3,
+                        "line": 6,
+                      },
+                      "start": Object {
+                        "character": 0,
+                        "line": 0,
+                      },
+                    },
+                  },
+                  "range": Object {
+                    "end": Object {
+                      "character": 3,
+                      "line": 6,
+                    },
+                    "start": Object {
+                      "character": 0,
+                      "line": 3,
+                    },
+                  },
+                },
+                "range": Object {
+                  "end": Object {
+                    "character": 3,
+                    "line": 6,
+                  },
+                  "start": Object {
+                    "character": 0,
+                    "line": 3,
+                  },
+                },
+              },
+              "range": Object {
+                "end": Object {
+                  "character": 3,
+                  "line": 6,
+                },
+                "start": Object {
+                  "character": 11,
+                  "line": 3,
+                },
+              },
+            },
+            "range": Object {
+              "end": Object {
+                "character": 24,
+                "line": 5,
+              },
+              "start": Object {
+                "character": 2,
+                "line": 4,
+              },
+            },
+          },
+          "range": Object {
+            "end": Object {
+              "character": 24,
+              "line": 5,
+            },
+            "start": Object {
+              "character": 2,
+              "line": 5,
+            },
+          },
+        },
+        "range": Object {
+          "end": Object {
+            "character": 24,
+            "line": 5,
+          },
+          "start": Object {
+            "character": 2,
+            "line": 5,
+          },
+        },
+      },
+      "range": Object {
+        "end": Object {
+          "character": 24,
+          "line": 5,
+        },
+        "start": Object {
+          "character": 8,
+          "line": 5,
+        },
+      },
+    },
+    "range": Object {
+      "end": Object {
+        "character": 24,
+        "line": 5,
+      },
+      "start": Object {
+        "character": 22,
+        "line": 5,
+      },
+    },
+  },
+]
+`)});
+
+  it("returns a selection range for functors", async () => {
+    openDocument(outdent`
+module M = Map.Make (struct
+  type t = { o: < rank : int > }
+  let compare a b = a.o#rank - b.o#rank
+end)`);
+
+  let result = await selectionRange([Types.Position.create(2, 26)]);
+  expect(result).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "parent": Object {
+      "parent": Object {
+        "parent": Object {
+          "parent": Object {
+            "parent": Object {
+              "parent": Object {
+                "parent": Object {
+                  "parent": Object {
+                    "parent": Object {
+                      "parent": Object {
+                        "parent": Object {
+                          "range": Object {
+                            "end": Object {
+                              "character": 4,
+                              "line": 3,
+                            },
+                            "start": Object {
+                              "character": 0,
+                              "line": 0,
+                            },
+                          },
+                        },
+                        "range": Object {
+                          "end": Object {
+                            "character": 4,
+                            "line": 3,
+                          },
+                          "start": Object {
+                            "character": 0,
+                            "line": 0,
+                          },
+                        },
+                      },
+                      "range": Object {
+                        "end": Object {
+                          "character": 4,
+                          "line": 3,
+                        },
+                        "start": Object {
+                          "character": 0,
+                          "line": 0,
+                        },
+                      },
+                    },
+                    "range": Object {
+                      "end": Object {
+                        "character": 4,
+                        "line": 3,
+                      },
+                      "start": Object {
+                        "character": 11,
+                        "line": 0,
+                      },
+                    },
+                  },
+                  "range": Object {
+                    "end": Object {
+                      "character": 3,
+                      "line": 3,
+                    },
+                    "start": Object {
+                      "character": 21,
+                      "line": 0,
+                    },
+                  },
+                },
+                "range": Object {
+                  "end": Object {
+                    "character": 39,
+                    "line": 2,
+                  },
+                  "start": Object {
+                    "character": 2,
+                    "line": 1,
+                  },
+                },
+              },
+              "range": Object {
+                "end": Object {
+                  "character": 39,
+                  "line": 2,
+                },
+                "start": Object {
+                  "character": 2,
+                  "line": 2,
+                },
+              },
+            },
+            "range": Object {
+              "end": Object {
+                "character": 39,
+                "line": 2,
+              },
+              "start": Object {
+                "character": 2,
+                "line": 2,
+              },
+            },
+          },
+          "range": Object {
+            "end": Object {
+              "character": 39,
+              "line": 2,
+            },
+            "start": Object {
+              "character": 14,
+              "line": 2,
+            },
+          },
+        },
+        "range": Object {
+          "end": Object {
+            "character": 39,
+            "line": 2,
+          },
+          "start": Object {
+            "character": 20,
+            "line": 2,
+          },
+        },
+      },
+      "range": Object {
+        "end": Object {
+          "character": 28,
+          "line": 2,
+        },
+        "start": Object {
+          "character": 20,
+          "line": 2,
+        },
+      },
+    },
+    "range": Object {
+      "end": Object {
+        "character": 28,
+        "line": 2,
+      },
+      "start": Object {
+        "character": 23,
+        "line": 2,
+      },
+    },
+  },
+]
+`)});
+
+it("returns a reasonable selection range for ill-typed modules", async () => {
+  openDocument(outdent`
+module M = struct
+    let f x : int = string_of_int x
+end`);
+
+let result = await selectionRange([Types.Position.create(1, 34)]);
+expect(result).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "parent": Object {
+      "parent": Object {
+        "parent": Object {
+          "parent": Object {
+            "parent": Object {
+              "parent": Object {
+                "parent": Object {
+                  "parent": Object {
+                    "parent": Object {
+                      "range": Object {
+                        "end": Object {
+                          "character": 3,
+                          "line": 2,
+                        },
+                        "start": Object {
+                          "character": 0,
+                          "line": 0,
+                        },
+                      },
+                    },
+                    "range": Object {
+                      "end": Object {
+                        "character": 3,
+                        "line": 2,
+                      },
+                      "start": Object {
+                        "character": 0,
+                        "line": 0,
+                      },
+                    },
+                  },
+                  "range": Object {
+                    "end": Object {
+                      "character": 3,
+                      "line": 2,
+                    },
+                    "start": Object {
+                      "character": 0,
+                      "line": 0,
+                    },
+                  },
+                },
+                "range": Object {
+                  "end": Object {
+                    "character": 3,
+                    "line": 2,
+                  },
+                  "start": Object {
+                    "character": 11,
+                    "line": 0,
+                  },
+                },
+              },
+              "range": Object {
+                "end": Object {
+                  "character": 35,
+                  "line": 1,
+                },
+                "start": Object {
+                  "character": 4,
+                  "line": 1,
+                },
+              },
+            },
+            "range": Object {
+              "end": Object {
+                "character": 35,
+                "line": 1,
+              },
+              "start": Object {
+                "character": 4,
+                "line": 1,
+              },
+            },
+          },
+          "range": Object {
+            "end": Object {
+              "character": 35,
+              "line": 1,
+            },
+            "start": Object {
+              "character": 4,
+              "line": 1,
+            },
+          },
+        },
+        "range": Object {
+          "end": Object {
+            "character": 35,
+            "line": 1,
+          },
+          "start": Object {
+            "character": 10,
+            "line": 1,
+          },
+        },
+      },
+      "range": Object {
+        "end": Object {
+          "character": 35,
+          "line": 1,
+        },
+        "start": Object {
+          "character": 20,
+          "line": 1,
+        },
+      },
+    },
+    "range": Object {
+      "end": Object {
+        "character": 35,
+        "line": 1,
+      },
+      "start": Object {
+        "character": 20,
+        "line": 1,
+      },
+    },
+  },
+]
+`)});
+
+it("returns a reasonable selection range in the presence of syntax errors", async () => {
+  openDocument(outdent`
+module M = struct
+    let f x : int = string_of_int x
+ed`);
+
+let result = await selectionRange([Types.Position.create(1, 34)]);
+expect(result).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "parent": Object {
+      "parent": Object {
+        "parent": Object {
+          "parent": Object {
+            "parent": Object {
+              "parent": Object {
+                "parent": Object {
+                  "parent": Object {
+                    "parent": Object {
+                      "range": Object {
+                        "end": Object {
+                          "character": 2,
+                          "line": 2,
+                        },
+                        "start": Object {
+                          "character": 0,
+                          "line": 0,
+                        },
+                      },
+                    },
+                    "range": Object {
+                      "end": Object {
+                        "character": 2,
+                        "line": 2,
+                      },
+                      "start": Object {
+                        "character": 0,
+                        "line": 0,
+                      },
+                    },
+                  },
+                  "range": Object {
+                    "end": Object {
+                      "character": 2,
+                      "line": 2,
+                    },
+                    "start": Object {
+                      "character": 0,
+                      "line": 0,
+                    },
+                  },
+                },
+                "range": Object {
+                  "end": Object {
+                    "character": 2,
+                    "line": 2,
+                  },
+                  "start": Object {
+                    "character": 11,
+                    "line": 0,
+                  },
+                },
+              },
+              "range": Object {
+                "end": Object {
+                  "character": 2,
+                  "line": 2,
+                },
+                "start": Object {
+                  "character": 4,
+                  "line": 1,
+                },
+              },
+            },
+            "range": Object {
+              "end": Object {
+                "character": 2,
+                "line": 2,
+              },
+              "start": Object {
+                "character": 4,
+                "line": 1,
+              },
+            },
+          },
+          "range": Object {
+            "end": Object {
+              "character": 2,
+              "line": 2,
+            },
+            "start": Object {
+              "character": 4,
+              "line": 1,
+            },
+          },
+        },
+        "range": Object {
+          "end": Object {
+            "character": 2,
+            "line": 2,
+          },
+          "start": Object {
+            "character": 10,
+            "line": 1,
+          },
+        },
+      },
+      "range": Object {
+        "end": Object {
+          "character": 2,
+          "line": 2,
+        },
+        "start": Object {
+          "character": 20,
+          "line": 1,
+        },
+      },
+    },
+    "range": Object {
+      "end": Object {
+        "character": 2,
+        "line": 2,
+      },
+      "start": Object {
+        "character": 20,
+        "line": 1,
+      },
+    },
+  },
+]
+`)});
 });
