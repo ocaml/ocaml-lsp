8b47925eb44f907b8ec41a44c1b2a55447f1b439
PixieDust <111846546+PizieDust@users.noreply.github.com>
2024-10-11 14:59:05 +0200

    Minor patch for Jump codeactions (#1391)

--- ocaml-lsp-server/src/code_actions.ml
+++ ocaml-lsp-server/src/code_actions.ml
@@ -118,8 +118,8 @@ let compute server (params : CodeActionParams.t) =
     let open_related = Action_open_related.for_uri capabilities doc in
     let* merlin_jumps =
       match state.configuration.data.merlin_jump_code_actions with
-      | Some { enable = true } -> Action_jump.code_actions doc params capabilities
-      | _ -> Fiber.return []
+      | Some { enable = true } | None -> Action_jump.code_actions doc params capabilities
+      | Some { enable = false } -> Fiber.return []
     in
     (match Document.syntax doc with
      | Ocamllex | Menhir | Cram | Dune ->
