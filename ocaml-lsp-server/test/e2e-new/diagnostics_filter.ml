acbbf3ce08b35f9d2f198840a836e503eb21ac69
xvw <xavier.vdw@gmail.com>
2025-04-08 22:09:01 +0200

    Add test

---
+++ ocaml-lsp-server/test/e2e-new/diagnostics_filter.ml
@@ -0,0 +1,113 @@
+open Test.Import
+
+let print_diagnostics
+      ?(prep = fun _ -> Fiber.return ())
+      ?(print_range = false)
+      ?(path = "foo.ml")
+      source
+  =
+  Lsp_helpers.open_document_with_diagnostics_callback
+    ~prep
+    ~path
+    ~source
+    ~diagnostics_callback:(fun diagnostics ->
+      print_endline
+        (String.concat
+           ~sep:", "
+           (List.map diagnostics.diagnostics ~f:(fun (d : Diagnostic.t) ->
+              let range_message =
+                if print_range
+                then "\n" ^ Ocaml_lsp_server.Testing.Range.to_string d.range ^ "\n"
+                else ""
+              in
+              match d.message with
+              | `String m -> m ^ range_message
+              | `MarkupContent { value; _ } -> value ^ range_message))))
+    ()
+;;
+
+let change_config client params = Client.notification client (ChangeConfiguration params)
+
+let%expect_test "Doesn't add other diagnostics if syntax errors" =
+  let source =
+    {ocaml|
+let x = "" in
+
+let () = 1    
+|ocaml}
+  in
+  print_diagnostics source;
+  [%expect {| Syntax error, expecting `in' |}]
+;;
+
+let%expect_test "shorten diagnostics - true" =
+  let source =
+    {ocaml|
+    let x: unit = fun () ->
+
+
+
+
+   ()
+
+   let () = match true with
+
+   | false -> ()
+|ocaml}
+  in
+  print_diagnostics
+    ~prep:(fun client ->
+      change_config
+        client
+        (DidChangeConfigurationParams.create
+           ~settings:
+             (`Assoc [ "shortenMerlinDiagnostics", `Assoc [ "enable", `Bool true ] ])))
+    ~print_range:true
+    source;
+  [%expect
+    {|
+    This expression should not be a function, the expected type is
+    unit
+    ((1, 18), (2, 0))
+    , Warning 8: this pattern-matching is not exhaustive.
+    Here is an example of a case that is not matched:
+    true
+    ((8, 12), (9, 0))
+    |}]
+;;
+
+let%expect_test "shorten diagnostics - false" =
+  let source =
+    {ocaml|
+    let x: unit = fun () ->
+
+
+
+
+   ()
+
+   let () = match true with
+
+   | false -> ()
+|ocaml}
+  in
+  print_diagnostics
+    ~prep:(fun client ->
+      change_config
+        client
+        (DidChangeConfigurationParams.create
+           ~settings:
+             (`Assoc [ "shortenMerlinDiagnostics", `Assoc [ "enable", `Bool false ] ])))
+    ~print_range:true
+    source;
+  [%expect
+    {|
+    This expression should not be a function, the expected type is
+    unit
+    ((1, 18), (6, 5))
+    , Warning 8: this pattern-matching is not exhaustive.
+    Here is an example of a case that is not matched:
+    true
+    ((8, 12), (10, 16))
+    |}]
+;;
--- ocaml-lsp-server/test/e2e-new/lsp_helpers.ml
+++ ocaml-lsp-server/test/e2e-new/lsp_helpers.ml
@@ -11,6 +11,42 @@ let open_document ~client ~uri ~source =
     (TextDocumentDidOpen (DidOpenTextDocumentParams.create ~textDocument))
 ;;
 
+let create_handler_with_diagnostics_callbacks ~got_diagnostics ~diagnostics_callback =
+  Client.Handler.make ~on_notification:(fun _ -> function
+    | PublishDiagnostics diagnostics ->
+      let () = diagnostics_callback diagnostics in
+      let* diag = Fiber.Ivar.peek got_diagnostics in
+      (match diag with
+       | Some _ -> Fiber.return ()
+       | None -> Fiber.Ivar.fill got_diagnostics ())
+    | _ -> Fiber.return ())
+;;
+
+let create_client client =
+  let run_client () =
+    let capabilities =
+      let window =
+        let showDocument = ShowDocumentClientCapabilities.create ~support:true in
+        WindowClientCapabilities.create ~showDocument ()
+      in
+      ClientCapabilities.create ~window ()
+    in
+    Client.start client (InitializeParams.create ~capabilities ())
+  in
+  run_client
+;;
+
+let open_document_with_client ~prep ~path ~source client =
+  let* _ = Client.initialized client in
+  let* () =
+    let settings = `Assoc [ "merlinDiagnostics", `Assoc [ "enable", `Bool true ] ] in
+    Client.notification client (ChangeConfiguration { settings })
+  in
+  let uri = DocumentUri.of_path path in
+  let* () = prep client in
+  open_document ~client ~uri ~source
+;;
+
 let iter_lsp_response
       ?(prep = fun _ -> Fiber.return ())
       ?(path = "foo.ml")
@@ -58,4 +94,23 @@ let iter_lsp_response
   in
   Fiber.fork_and_join_unit run_client (fun () ->
     run >>> Fiber.Ivar.read got_diagnostics >>> Client.stop client)
+;;
+
+let open_document_with_diagnostics_callback
+      ?(prep = fun _ -> Fiber.return ())
+      ?(path = "foo.ml")
+      ~source
+      ~diagnostics_callback
+      ()
+  =
+  let got_diagnostics = Fiber.Ivar.create () in
+  let handler =
+    create_handler_with_diagnostics_callbacks ~got_diagnostics ~diagnostics_callback ()
+  in
+  Test.run ~handler
+  @@ fun client ->
+  let run_client = create_client client in
+  let open_document = open_document_with_client ~prep ~path ~source client in
+  Fiber.fork_and_join_unit run_client (fun () ->
+    open_document >>> Fiber.Ivar.read got_diagnostics >>> Client.stop client)
 ;;
