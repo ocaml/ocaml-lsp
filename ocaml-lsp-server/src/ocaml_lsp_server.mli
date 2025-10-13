8b43dad4c8185880f2267e43ac9133fbe8fc54af
Ulysse Gérard <thevoodoos@gmail.com>
2025-10-13 11:00:27 +0200

    More natural fallback on .merlin files when no dune-project is found.
    Implements the behaviour described in issue #1522, fixes #1522

--- ocaml-lsp-server/src/ocaml_lsp_server.mli
+++ ocaml-lsp-server/src/ocaml_lsp_server.mli
@@ -1,4 +1,4 @@
-val run : Lsp.Cli.Channel.t -> read_dot_merlin:bool -> unit -> unit
+val run : Lsp.Cli.Channel.t -> prefer_dot_merlin:bool -> unit -> unit
 
 module Diagnostics = Diagnostics
 module Version = Version
