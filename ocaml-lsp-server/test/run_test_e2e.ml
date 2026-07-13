85900bd65b7dffe02d07959a97d9e8c7a4350fd2
Rudi Grinberg <me@rgrinberg.com>
2026-07-13 21:41:33 +0100

    test(e2e): port folding range tests
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/run_test_e2e.ml
+++ ocaml-lsp-server/test/run_test_e2e.ml
@@ -1,6 +1,8 @@
 let () =
   let cmd =
-    Filename.quote_command "yarn" [ "--cwd"; "ocaml-lsp-server/test/e2e"; "test" ]
+    Filename.quote_command
+      "yarn"
+      [ "--cwd"; "ocaml-lsp-server/test/e2e"; "test"; "--passWithNoTests" ]
   in
   Sys.command cmd |> exit
 ;;
