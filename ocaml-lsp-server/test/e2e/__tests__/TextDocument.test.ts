214068ae97942fedcd79b107944f1148c50ece67
Rudi Grinberg <me@rgrinberg.com>
2026-07-13 12:31:44 +0100

    test(e2e): port text document didOpen stores document test
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e/__tests__/TextDocument.test.ts
+++ ocaml-lsp-server/test/e2e/__tests__/TextDocument.test.ts
@@ -10,32 +10,6 @@ describe("TextDocument", () => {
   });
 
   describe("didOpen", () => {
-    it("stores text document", async () => {
-      languageServer = await LanguageServer.startAndInitialize();
-      languageServer.sendNotification(
-        Protocol.DidOpenTextDocumentNotification.type,
-        {
-          textDocument: Types.TextDocumentItem.create(
-            "file:///test-document.txt",
-            "ocaml",
-            0,
-            "Hello, World!",
-          ),
-        },
-      );
-
-      const result: string = await languageServer.sendRequest(
-        "debug/textDocument/get",
-        {
-          textDocument: Types.TextDocumentIdentifier.create(
-            "file:///test-document.txt",
-          ),
-          position: Types.Position.create(0, 0),
-        },
-      );
-
-      expect(result).toEqual("Hello, World!");
-    });
   });
 
   describe("didChange", () => {
