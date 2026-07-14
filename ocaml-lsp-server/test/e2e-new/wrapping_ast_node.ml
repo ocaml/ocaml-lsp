308b04aa44f8a5b5af1926c7e280f6e53c474909
Rudi Grinberg <me@rgrinberg.com>
2026-07-15 07:53:49 +0800

    test(e2e): port wrapping AST node empty-document test (#1655)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ ocaml-lsp-server/test/e2e-new/wrapping_ast_node.ml
@@ -0,0 +1,43 @@
+open Test.Import
+module Req = Ocaml_lsp_server.Custom_request.Wrapping_ast_node
+
+let wrapping_ast_node client position =
+  let params =
+    `Assoc
+      [ "uri", DocumentUri.yojson_of_t Helpers.uri
+      ; "position", Position.yojson_of_t position
+      ]
+    |> Jsonrpc.Structured.t_of_yojson
+    |> Option.some
+  in
+  Client.request client (UnknownRequest { meth = Req.meth; params })
+;;
+
+let print_json json = Yojson.Safe.pretty_to_string ~std:false json |> print_endline
+
+let test source position =
+  let req client =
+    let* response = wrapping_ast_node client position in
+    print_json response;
+    Fiber.return ()
+  in
+  Helpers.test source req
+;;
+
+let code_snippet_0 =
+  {ocaml|let k = 1
+
+module M = struct
+  let a =
+    let b = 1 in
+    b + 1
+
+  let c = 2
+end
+|ocaml}
+;;
+
+let%expect_test "empty document" =
+  test "" (Position.create ~line:0 ~character:0);
+  [%expect {| null |}]
+;;
