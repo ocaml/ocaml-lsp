9c639a38cd20347da65ae26b594132779b7912aa
Rudi Grinberg <me@rgrinberg.com>
2026-07-14 09:05:33 +0800

    chore: more [Stdune] removal (#1644)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/import.ml
+++ ocaml-lsp-server/src/import.ml
@@ -122,8 +122,33 @@ module Option = struct
   include Base.Option
 end
 
+module Env_vars = struct
+  open Option.O
+
+  let _TEST () : bool option =
+    let+ v = Sys.getenv_opt "OCAMLLSP_TEST" in
+    match v with
+    | "true" -> true
+    | "false" -> false
+    | unexpected_val ->
+      Format.eprintf
+        "invalid value %S for OCAMLLSP_TEST ignored. Only true or false are allowed@."
+        unexpected_val;
+      false
+  ;;
+
+  let _IS_HOVER_EXTENDED () : bool option =
+    let* v = Sys.getenv_opt "OCAMLLSP_HOVER_IS_EXTENDED" in
+    match v with
+    | "true" | "1" -> Some true
+    | _ -> Some false
+  ;;
+end
+
 module String = struct
   type t = string
+
+  let starts_with = Stdlib.String.starts_with
 
   module Map = struct
     include MoreLabels.Map.Make (Stdlib.String)
