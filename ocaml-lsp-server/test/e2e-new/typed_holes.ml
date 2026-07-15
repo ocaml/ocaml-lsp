5126a1c7e3de05d90de8e01cb1e842da39b932b5
Rudi Grinberg <me@rgrinberg.com>
2026-07-15 22:51:08 +0800

    test(e2e): port typed holes multiple-holes test (#1672)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/typed_holes.ml
+++ ocaml-lsp-server/test/e2e-new/typed_holes.ml
@@ -47,3 +47,36 @@ let%expect_test "one hole" =
     ]
     |}]
 ;;
+
+let%expect_test "several holes" =
+  let source =
+    {ocaml|let u =
+  let i = match Some 1 with None -> _ | Some -> _ in
+  let b = match () with () -> _ in
+  ()
+|ocaml}
+  in
+  let req client =
+    let* response = typed_holes client in
+    print_json response;
+    Fiber.return ()
+  in
+  Helpers.test source req;
+  [%expect
+    {|
+    [
+      {
+        "end": { "character": 31, "line": 2 },
+        "start": { "character": 30, "line": 2 }
+      },
+      {
+        "end": { "character": 37, "line": 1 },
+        "start": { "character": 36, "line": 1 }
+      },
+      {
+        "end": { "character": 49, "line": 1 },
+        "start": { "character": 48, "line": 1 }
+      }
+    ]
+    |}]
+;;
