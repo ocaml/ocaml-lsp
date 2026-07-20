939e14b7f7b59d91985cc348e1c2dc9b81e58e5a
Rudi Grinberg <me@rgrinberg.com>
2026-07-20 23:13:43 +0800

    test(lsp): reproduce negative array-view mutation regression (#1766)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/array_view_tests.ml
+++ lsp/test/array_view_tests.ml
@@ -15,3 +15,14 @@ let%expect_test "subviews stay within their parent view" =
    | exception Invalid_argument message -> Printf.printf "invalid: %s\n" message);
   [%expect {| value: 3 |}]
 ;;
+
+let%expect_test "negative mutations do not escape an array view" =
+  let backing = [| 0; 1; 2; 3 |] in
+  let view = Array_view.make backing ~pos:1 ~len:2 in
+  (match Array_view.set view (-1) 9 with
+   | () ->
+     Array.iter (Printf.printf "%d ") backing;
+     print_newline ()
+   | exception Invalid_argument message -> Printf.printf "invalid: %s\n" message);
+  [%expect {| 9 1 2 3 |}]
+;;
