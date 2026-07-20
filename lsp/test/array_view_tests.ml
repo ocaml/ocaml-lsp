e754be665eae00e61b818bb66169b5002d6fa68a
Rudi Grinberg <me@rgrinberg.com>
2026-07-20 01:49:08 +0100

    test(lsp): reproduce escaping array-subview regression
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/array_view_tests.ml
+++ lsp/test/array_view_tests.ml
@@ -7,3 +7,11 @@ let%expect_test "negative indices do not escape an array view" =
    | exception Invalid_argument message -> Printf.printf "invalid: %s\n" message);
   [%expect {| value: 0 |}]
 ;;
+
+let%expect_test "subviews stay within their parent view" =
+  let parent = Array_view.make [| 0; 1; 2; 3; 4 |] ~pos:1 ~len:2 in
+  (match Array_view.sub parent ~pos:2 ~len:1 with
+   | child -> Printf.printf "value: %d\n" (Array_view.get child 0)
+   | exception Invalid_argument message -> Printf.printf "invalid: %s\n" message);
+  [%expect {| value: 3 |}]
+;;
