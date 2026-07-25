e889d53a1049aedeffa3d5c8b1b04c817d2a0533
Rudi Grinberg <me@rgrinberg.com>
2026-07-25 04:54:11 +0100

    test(server): reproduce unsupported requests as internal errors
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ ocaml-lsp-server/test/e2e-new/unsupported_requests.ml
@@ -0,0 +1,28 @@
+open Test.Import
+
+let%expect_test "unsupported standard requests are reported as internal errors" =
+  (Test.run_initialized
+   @@ fun client ->
+   let textDocument =
+     TextDocumentIdentifier.create ~uri:(DocumentUri.of_path "unsupported.ml")
+   in
+   let position = Position.create ~line:0 ~character:0 in
+   let params = LinkedEditingRangeParams.create ~textDocument ~position () in
+   let* response =
+     Fiber.collect_errors (fun () -> Client.request client (LinkedEditingRange params))
+   in
+   let* () =
+     match response with
+     | Error
+         [ { Exn_with_backtrace.exn = Jsonrpc.Response.Error.E error; backtrace = _ } ] ->
+       Jsonrpc.Response.Error.yojson_of_t error |> Test.print_result;
+       Fiber.return ()
+     | Error errors -> Fiber.reraise_all errors
+     | Ok _ -> failwith "unsupported request unexpectedly succeeded"
+   in
+   Test.shutdown_client client);
+  [%expect
+    {|
+    { "code": -32603, "message": "Request not supported yet!" }
+    |}]
+;;
