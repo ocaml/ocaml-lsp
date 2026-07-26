da77815b35cbe215a3e725fa4f74b67ed15e8c2b
Rudi Grinberg <me@rgrinberg.com>
2026-07-26 13:02:50 +0800

    test(e2e): split slow test partitions (#1893)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ ocaml-lsp-server/test/e2e-new/code_actions_annotations.ml
@@ -0,0 +1,455 @@
+open Test.Import
+open Lsp_helpers
+open Code_actions
+
+let%expect_test "no code actions for dune documents" =
+  let source = "(library (name foo))\n" in
+  let range = range ~start_line:0 ~start_character:0 ~end_line:0 ~end_character:0 in
+  let makeRequest textDocument =
+    let context = CodeActionContext.create ~diagnostics:[] () in
+    Lsp.Client_request.CodeAction
+      (CodeActionParams.create ~textDocument ~range ~context ())
+  in
+  iter_lsp_response
+    ~path:"dune"
+    ~language_id:"dune"
+    ~makeRequest
+    ~source
+    print_code_action_result;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "code actions" =
+  let source =
+    {ocaml|
+let foo = 123
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:5 ~end_line:1 ~end_character:7 in
+  print_code_actions source range;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(foo : int)",
+                "range": {
+                  "end": { "character": 7, "line": 1 },
+                  "start": { "character": 4, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "type-annotate",
+      "title": "Type-annotate"
+    }
+    {
+      "command": {
+        "arguments": [ "file:///foo.mli" ],
+        "command": "ocamllsp/open-related-source",
+        "title": "Create foo.mli"
+      },
+      "edit": {
+        "documentChanges": [ { "kind": "create", "uri": "file:///foo.mli" } ]
+      },
+      "kind": "switch",
+      "title": "Create foo.mli"
+    } |}]
+;;
+
+let%expect_test "code action only includes nested kinds" =
+  let source =
+    {ocaml|let _ =
+  let x = 0 in
+  x + 1
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:6 ~end_line:1 ~end_character:7 in
+  print_code_actions ~only:[ CodeActionKind.Refactor ] source range;
+  [%expect
+    {|
+    Code actions:
+    {
+      "command": {
+        "arguments": [ "file:///foo.mli" ],
+        "command": "ocamllsp/open-related-source",
+        "title": "Create foo.mli"
+      },
+      "edit": {
+        "documentChanges": [ { "kind": "create", "uri": "file:///foo.mli" } ]
+      },
+      "kind": "switch",
+      "title": "Create foo.mli"
+    } |}]
+;;
+
+let%expect_test "can type-annotate a function argument" =
+  let source =
+    {ocaml|
+type t = Foo of int | Bar of bool
+let f x = Foo x
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:6 ~end_line:2 ~end_character:7 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(x : int)",
+                "range": {
+                  "end": { "character": 7, "line": 2 },
+                  "start": { "character": 6, "line": 2 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "type-annotate",
+      "title": "Type-annotate"
+    } |}]
+;;
+
+let%expect_test "can type-annotate a toplevel value" =
+  let source =
+    {ocaml|
+let iiii = 3 + 4
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:4 ~end_line:1 ~end_character:5 in
+  print_code_actions source range;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(iiii : int)",
+                "range": {
+                  "end": { "character": 8, "line": 1 },
+                  "start": { "character": 4, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "type-annotate",
+      "title": "Type-annotate"
+    }
+    {
+      "command": {
+        "arguments": [ "file:///foo.mli" ],
+        "command": "ocamllsp/open-related-source",
+        "title": "Create foo.mli"
+      },
+      "edit": {
+        "documentChanges": [ { "kind": "create", "uri": "file:///foo.mli" } ]
+      },
+      "kind": "switch",
+      "title": "Create foo.mli"
+    }
+     |}]
+;;
+
+let%expect_test "does not type-annotate function" =
+  let source =
+    {ocaml|
+let my_fun x y = 1
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:5 ~end_line:1 ~end_character:6 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "can type-annotate an argument in a function call" =
+  let source =
+    {ocaml|
+let f x = x + 1
+let () =
+  let i = 8 in
+  print_int (f i)
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:7 ~end_line:1 ~end_character:8 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(x : int)",
+                "range": {
+                  "end": { "character": 7, "line": 1 },
+                  "start": { "character": 6, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "type-annotate",
+      "title": "Type-annotate"
+    } |}]
+;;
+
+let%expect_test "can type-annotate a variant with its name only" =
+  let source =
+    {ocaml|
+type t = Foo of int | Bar of bool
+
+let f (x : t) = x
+|ocaml}
+  in
+  let range = range ~start_line:3 ~start_character:16 ~end_line:3 ~end_character:17 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(x : t)",
+                "range": {
+                  "end": { "character": 17, "line": 3 },
+                  "start": { "character": 16, "line": 3 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "type-annotate",
+      "title": "Type-annotate"
+    } |}]
+;;
+
+let%expect_test "does not type-annotate in a non expression context" =
+  let source =
+    {ocaml|
+type x =
+   | Foo of int
+   | Baz of string
+|ocaml}
+  in
+  let range = range ~start_line:3 ~start_character:5 ~end_line:3 ~end_character:6 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "does not type-annotate already annotated argument" =
+  let source =
+    {ocaml|
+let f (x : int) = 1
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:7 ~end_line:1 ~end_character:8 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "does not type-annotate already annotated expression" =
+  let source =
+    {ocaml|
+let f x = (1 : int)
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:11 ~end_line:1 ~end_character:12 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "does not type-annotate already annotated and coerced expression" =
+  let source =
+    {ocaml|
+let f x = (1 : int :> int)
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:11 ~end_line:1 ~end_character:12 in
+  print_code_actions source range ~filter:find_annotate_action;
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "can remove type annotation from a function argument" =
+  let source =
+    {ocaml|
+type t = Foo of int | Bar of bool
+let f (x : t) = Foo x
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:7 ~end_line:2 ~end_character:8 in
+  print_code_actions source range ~filter:find_remove_annotation_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "x",
+                "range": {
+                  "end": { "character": 13, "line": 2 },
+                  "start": { "character": 6, "line": 2 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "remove type annotation",
+      "title": "Remove type annotation"
+    } |}]
+;;
+
+let%expect_test "can remove type annotation from a toplevel value" =
+  let source =
+    {ocaml|
+let (iiii : int) = 3 + 4
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:5 ~end_line:1 ~end_character:6 in
+  print_code_actions source range ~filter:find_remove_annotation_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "iiii",
+                "range": {
+                  "end": { "character": 16, "line": 1 },
+                  "start": { "character": 4, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "remove type annotation",
+      "title": "Remove type annotation"
+    } |}]
+;;
+
+let%expect_test "can remove type annotation from an argument in a function call" =
+  let source =
+    {ocaml|
+let f (x : int) = x + 1
+ let () =
+   let i = 8 in
+   print_int (f i)
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:7 ~end_line:1 ~end_character:8 in
+  print_code_actions source range ~filter:find_remove_annotation_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "x",
+                "range": {
+                  "end": { "character": 15, "line": 1 },
+                  "start": { "character": 6, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "remove type annotation",
+      "title": "Remove type annotation"
+    } |}]
+;;
+
+let%expect_test "can remove type annotation from a coerced expression" =
+  let source =
+    {ocaml|
+let x = (7 : int :> int)
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:9 ~end_line:1 ~end_character:10 in
+  print_code_actions source range ~filter:find_remove_annotation_action;
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "7",
+                "range": {
+                  "end": { "character": 16, "line": 1 },
+                  "start": { "character": 9, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "remove type annotation",
+      "title": "Remove type annotation"
+    } |}]
+;;
+
+let%expect_test "does not remove type annotation from function" =
+  let source =
+    {ocaml|
+let my_fun x y : int = 1
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:5 ~end_line:1 ~end_character:6 in
+  print_code_actions source range ~filter:find_remove_annotation_action;
+  [%expect {| No code actions |}]
+;;
---
+++ ocaml-lsp-server/test/e2e-new/code_actions_destruct.ml
@@ -0,0 +1,542 @@
+open Test.Import
+open Lsp_helpers
+open Code_actions
+
+let%expect_test "can destruct sum types" =
+  let source =
+    {ocaml|
+type t = Foo of int | Bar of bool
+let f (x : t) = x
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:16 ~end_line:2 ~end_character:17 in
+  print_code_actions source range ~filter:(find_action "destruct (enumerate cases)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "match x with | Foo _ -> _ | Bar _ -> _",
+                "range": {
+                  "end": { "character": 17, "line": 2 },
+                  "start": { "character": 16, "line": 2 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct (enumerate cases)",
+      "title": "Destruct (enumerate cases)"
+    }
+    |}]
+;;
+
+let%expect_test "can destruct match line" =
+  let source =
+    {ocaml|
+let f (x:bool) =
+  match x
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:5 ~end_line:2 ~end_character:5 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "match x with\n  | false -> _\n  | true -> _",
+                "range": {
+                  "end": { "character": 9, "line": 2 },
+                  "start": { "character": 2, "line": 2 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "destruct-line is unavailable on a whole inline match expression" =
+  let source =
+    {ocaml|
+type t = A | B | C
+let x = match A with
+  | A -> _
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:8 ~end_line:3 ~end_character:10 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "destruct-line is unavailable when a match case is on the same line" =
+  let source =
+    {ocaml|
+type t = A | B | C
+let x = match A with | A -> _
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:8 ~end_line:2 ~end_character:13 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "destruct-line returns UTF-16 edit ranges" =
+  let source =
+    {ocaml|
+let f (café : bool) =
+  match café
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:10 ~end_line:2 ~end_character:10 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "match café with\n  | false -> _\n  | true -> _",
+                "range": {
+                  "end": { "character": 13, "line": 2 },
+                  "start": { "character": 2, "line": 2 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "destruct-line selects an expression repeated in match" =
+  let source =
+    {ocaml|
+let f (a : bool) =
+  match a
+|ocaml}
+  in
+  let range = range ~start_line:2 ~start_character:8 ~end_line:2 ~end_character:8 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect {| No code actions |}]
+;;
+
+let%expect_test "can destruct match-with line" =
+  let source =
+    {ocaml|
+    match (Ok 0) with
+|ocaml}
+  in
+  let range = range ~start_line:1 ~start_character:0 ~end_line:1 ~end_character:0 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "match Ok 0 with\n    | Ok _ -> _\n    | Error _ -> _",
+                "range": {
+                  "end": { "character": 21, "line": 1 },
+                  "start": { "character": 4, "line": 1 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "can destruct case line" =
+  let source =
+    {ocaml|
+type q =
+| A
+| B
+| C
+| D
+let f (x: q) =
+  match x with
+  | C -> _
+|ocaml}
+  in
+  let range = range ~start_line:8 ~start_character:0 ~end_line:8 ~end_character:0 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "\n  | A -> _\n  | B -> _\n  | D -> _",
+                "range": {
+                  "end": { "character": 10, "line": 8 },
+                  "start": { "character": 10, "line": 8 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "can destruct hole" =
+  let source =
+    {ocaml|
+let zip (type a b) (xs : a list) (ys : b list) : (a * b) list =
+  match (xs, ys) with
+  | (_, _) -> _
+|ocaml}
+  in
+  let range = range ~start_line:3 ~start_character:5 ~end_line:3 ~end_character:5 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "([], _) -> _\n  | (_::_, _)",
+                "range": {
+                  "end": { "character": 10, "line": 3 },
+                  "start": { "character": 4, "line": 3 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "destruct hole spacing" =
+  let source =
+    {ocaml|
+type q =
+| A
+| B
+| C
+| D
+let f (x: q) =
+  match x with
+  | _ -> _
+|ocaml}
+  in
+  let range = range ~start_line:8 ~start_character:5 ~end_line:8 ~end_character:5 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "A -> _\n  | B -> _\n  | C -> _\n  | D",
+                "range": {
+                  "end": { "character": 5, "line": 8 },
+                  "start": { "character": 4, "line": 8 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "destruct a case with a hole but not on the hole" =
+  let source =
+    {ocaml|
+type q =
+| A
+| B
+| C
+| D
+let f (x: q) =
+  match x with
+  | _ -> _
+|ocaml}
+  in
+  let range = range ~start_line:8 ~start_character:2 ~end_line:8 ~end_character:2 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "A -> _\n  | B -> _\n  | C -> _\n  | D",
+                "range": {
+                  "end": { "character": 5, "line": 8 },
+                  "start": { "character": 4, "line": 8 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct-line (enumerate cases, use existing match)",
+      "title": "Destruct-line (enumerate cases, use existing match)"
+    }
+    |}]
+;;
+
+let%expect_test "destruct uses the right number of newlines" =
+  let source =
+    {ocaml|
+type t =
+  | Very_long_name_for_for_the_first_case_so_that_merlin_will_use_multiple_lines
+  | Almost_as_long_name_for_for_the_second_case
+  | Another_long_name_for_for_the_third_case
+;;
+let f (x: t) =
+  match x with
+  |ocaml}
+  in
+  let range = range ~start_line:7 ~start_character:7 ~end_line:7 ~end_character:7 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+      Code actions:
+      {
+        "edit": {
+          "documentChanges": [
+            {
+              "edits": [
+                {
+                  "newText": "match x with\n  | Very_long_name_for_for_the_first_case_so_that_merlin_will_use_multiple_lines -> _\n  | Almost_as_long_name_for_for_the_second_case -> _\n  | Another_long_name_for_for_the_third_case -> _",
+                  "range": {
+                    "end": { "character": 14, "line": 7 },
+                    "start": { "character": 2, "line": 7 }
+                  }
+                }
+              ],
+              "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+            }
+          ]
+        },
+        "isPreferred": false,
+        "kind": "destruct-line (enumerate cases, use existing match)",
+        "title": "Destruct-line (enumerate cases, use existing match)"
+      }
+      |}]
+;;
+
+let%expect_test "destruct strips parentheses even on long lines" =
+  let source =
+    {ocaml|
+type q =
+  | Very_long_name_for_for_the_first_case_so_that_merlin_will_be_forced_to_use_multiple_lines
+  | Almost_as_long_name_for_for_the_second_case
+  | Another_long_name_for_for_the_third_case
+  | Very_long_name_for_for_the_last_case_so_that_we_can_make_sure_we_handle_both_parens_and_line_breaks of int
+;;
+let f (x: q) =
+  match x with
+  | Almost_as_long_name_for_for_the_second_case -> _
+|ocaml}
+  in
+  let range = range ~start_line:9 ~start_character:22 ~end_line:9 ~end_character:22 in
+  print_code_actions
+    source
+    range
+    ~filter:(find_action "destruct-line (enumerate cases, use existing match)");
+  [%expect
+    {|
+      Code actions:
+      {
+        "edit": {
+          "documentChanges": [
+            {
+              "edits": [
+                {
+                  "newText": "\n  | Very_long_name_for_for_the_first_case_so_that_merlin_will_be_forced_to_use_multiple_lines -> _\n  | Another_long_name_for_for_the_third_case -> _\n  | Very_long_name_for_for_the_last_case_so_that_we_can_make_sure_we_handle_both_parens_and_line_breaks\n     _ -> _",
+                  "range": {
+                    "end": { "character": 52, "line": 9 },
+                    "start": { "character": 52, "line": 9 }
+                  }
+                }
+              ],
+              "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+            }
+          ]
+        },
+        "isPreferred": false,
+        "kind": "destruct-line (enumerate cases, use existing match)",
+        "title": "Destruct-line (enumerate cases, use existing match)"
+      }
+      |}]
+;;
+
+let%expect_test "can destruct on sub-expression" =
+  let source =
+    {ocaml|
+let defered_peek x = if x >= 0 then Some (`Foo x) else None
+let job_reader = 10
+
+let _ = defered_peek job_reader
+|ocaml}
+  in
+  let range = range ~start_line:4 ~start_character:8 ~end_line:4 ~end_character:31 in
+  print_code_actions source range ~filter:(find_action "destruct (enumerate cases)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "match defered_peek job_reader with | None -> _ | Some _ -> _",
+                "range": {
+                  "end": { "character": 31, "line": 4 },
+                  "start": { "character": 8, "line": 4 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct (enumerate cases)",
+      "title": "Destruct (enumerate cases)"
+    }
+    |}]
+;;
+
+let%expect_test "can destruct on sub-expression that need parenthesis" =
+  let source =
+    {ocaml|
+let defered_peek x = if x >= 0 then Some (`Foo x) else None
+let job_reader = 10
+
+let _ = defered_peek job_reader
+|ocaml}
+  in
+  let range = range ~start_line:4 ~start_character:21 ~end_line:4 ~end_character:31 in
+  print_code_actions source range ~filter:(find_action "destruct (enumerate cases)");
+  [%expect
+    {|
+    Code actions:
+    {
+      "edit": {
+        "documentChanges": [
+          {
+            "edits": [
+              {
+                "newText": "(match job_reader with | 0 -> _ | _ -> _)",
+                "range": {
+                  "end": { "character": 31, "line": 4 },
+                  "start": { "character": 21, "line": 4 }
+                }
+              }
+            ],
+            "textDocument": { "uri": "file:///foo.ml", "version": 0 }
+          }
+        ]
+      },
+      "isPreferred": false,
+      "kind": "destruct (enumerate cases)",
+      "title": "Destruct (enumerate cases)"
+    }
+    |}]
+;;
