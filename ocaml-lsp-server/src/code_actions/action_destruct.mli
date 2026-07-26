adf815a723e9c58ca0f159a7a8cd138f292c9b15
Rudi Grinberg <me@rgrinberg.com>
2026-07-26 06:25:43 +0100

    refactor(code-actions): share destruct action runner
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/code_actions/action_destruct.mli
+++ ocaml-lsp-server/src/code_actions/action_destruct.mli
@@ -1,4 +1,16 @@
 open Import
 
 val kind : CodeActionKind.t
+
+(** Run Merlin's case analysis and turn its reply into a code action.
+    [postprocess] may adjust the replacement location and text. *)
+val run
+  :  State.t
+  -> Document.t
+  -> Document.Merlin.t
+  -> action_kind:string
+  -> range:Range.t
+  -> postprocess:(Loc.t * string -> Loc.t * string)
+  -> CodeAction.t option Fiber.t
+
 val t : State.t -> Code_action.t
