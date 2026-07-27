0bc0e8be2dfef86c906b3db3ca0b1fc606ad05c4
Rudi Grinberg <me@rgrinberg.com>
2026-07-27 09:19:52 +0100

    fix(lsp-fiber): memoize concurrent lazy fiber forces
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp-fiber/src/lazy_fiber.ml
+++ lsp-fiber/src/lazy_fiber.ml
@@ -1,18 +1,30 @@
+type 'a result = ('a, Stdune.Exn_with_backtrace.t list) Result.t
+
 type 'a t =
-  { value : 'a Fiber.Ivar.t
+  { value : 'a result Fiber.Ivar.t
   ; mutable f : (unit -> 'a Fiber.t) option
   }
 
 let create f = { f = Some f; value = Fiber.Ivar.create () }
 
+let await t =
+  let open Fiber.O in
+  Fiber.Ivar.read t.value
+  >>= function
+  | Ok value -> Fiber.return value
+  | Error errors -> Fiber.reraise_all errors
+;;
+
 let force t =
   let open Fiber.O in
-  match t.f with
-  | None -> Fiber.Ivar.read t.value
-  | Some f ->
-    Fiber.of_thunk (fun () ->
+  Fiber.of_thunk (fun () ->
+    match t.f with
+    | None -> await t
+    | Some f ->
       t.f <- None;
-      let* v = f () in
-      let+ () = Fiber.Ivar.fill t.value v in
-      v)
+      let* result = Fiber.collect_errors f in
+      let* () = Fiber.Ivar.fill t.value result in
+      (match result with
+       | Ok value -> Fiber.return value
+       | Error errors -> Fiber.reraise_all errors))
 ;;
