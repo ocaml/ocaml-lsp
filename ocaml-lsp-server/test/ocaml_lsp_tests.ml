cf4c48c44b7f42bb08e42eb534c27b4d7d1ffbf4
Rudi Grinberg <me@rgrinberg.com>
2026-07-27 16:05:35 +0800

    chore: simplify unit tests with existing helpers (#1916)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/ocaml_lsp_tests.ml
+++ ocaml-lsp-server/test/ocaml_lsp_tests.ml
@@ -1,4 +1,6 @@
 open Lsp.Types
+module Position = Lsp.Position
+module Range = Lsp.Range
 
 let%test_unit "convert an LSP position to a Merlin logical position" =
   let position = Position.create ~line:2 ~character:3 in
@@ -10,7 +12,7 @@ let%expect_test "replacement ranges preserve trailing newlines" =
   let range = Range.create ~start ~end_:start in
   let edit = TextEdit.create ~range ~newText:"x\n" in
   let range = Ocaml_lsp_server.Testing.Range.resize_for_edit edit in
-  print_endline (Lsp.Range.to_string range);
+  print_endline (Range.to_string range);
   [%expect {| ((2, 5), (2, 6)) |}]
 ;;
 
@@ -19,7 +21,7 @@ let%expect_test "replacement ranges use UTF-16 character units" =
   let range = Range.create ~start ~end_:start in
   let edit = TextEdit.create ~range ~newText:"😀" in
   let range = Ocaml_lsp_server.Testing.Range.resize_for_edit edit in
-  print_endline (Lsp.Range.to_string range);
+  print_endline (Range.to_string range);
   [%expect {| ((2, 5), (2, 9)) |}]
 ;;
 
@@ -29,29 +31,26 @@ let%expect_test "document-symbol selection range relationships" =
     let end_ = Position.create ~line:end_line ~character:end_character in
     Range.create ~start ~end_
   in
-  let compare_position (x : Position.t) (y : Position.t) =
-    Stdlib.compare (x.line, x.character) (y.line, y.character)
-  in
   let full_range = range 1 2 3 8 in
   let relation = function
     | None -> "ghost"
     | Some (selection_range : Range.t) ->
-      if compare_position selection_range.start selection_range.end_ > 0
+      if Position.compare selection_range.start selection_range.end_ > 0
       then "invalid"
-      else if Lsp.Range.contains full_range selection_range
+      else if Range.contains full_range selection_range
       then "contained"
       else (
         let start =
-          if compare_position full_range.start selection_range.start >= 0
+          if Position.compare full_range.start selection_range.start >= 0
           then full_range.start
           else selection_range.start
         in
         let end_ =
-          if compare_position full_range.end_ selection_range.end_ <= 0
+          if Position.compare full_range.end_ selection_range.end_ <= 0
           then full_range.end_
           else selection_range.end_
         in
-        match compare_position start end_ with
+        match Position.compare start end_ with
         | n when n < 0 -> "overlap"
         | 0 -> "touch"
         | _ -> "disjoint")
@@ -99,7 +98,7 @@ let%expect_test "normalize document-symbol selection ranges" =
         ~range:full_range
         selection_range
     in
-    Printf.printf "%s: %s\n" label (Lsp.Range.to_string normalized)
+    Printf.printf "%s: %s\n" label (Range.to_string normalized)
   in
   print "contained" (Some (range 1 4 2 6));
   print "contained empty" (Some (range 2 4 2 4));
