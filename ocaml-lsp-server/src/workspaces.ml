c0399a455289a755799c158a6824dc13289c3312
Rudi Grinberg <me@rgrinberg.com>
2026-07-27 18:10:12 +0100

    refactor(server): use Base maps for URIs
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/src/workspaces.ml
+++ ocaml-lsp-server/src/workspaces.ml
@@ -1,8 +1,7 @@
 open Import
-module Uri_map = Stdlib.MoreLabels.Map.Make (Uri)
 
 type t =
-  { workspace_folders : WorkspaceFolder.t Uri_map.t option
+  { workspace_folders : WorkspaceFolder.t Map.M(Uri).t option
   ; root_uri : Uri.t option
   ; root_path : string option
   }
@@ -13,27 +12,23 @@ let create (ip : InitializeParams.t) =
     | None | Some None -> None
     | Some (Some workspace_folders) ->
       List.map workspace_folders ~f:(fun (ws : WorkspaceFolder.t) -> ws.uri, ws)
-      |> Uri_map.of_list
+      |> Map.of_alist_reduce (module Uri) ~f:(fun _old new_ -> new_)
       |> Option.some
   in
   let root_uri = ip.rootUri in
-  let root_path =
-    match ip.rootPath with
-    | None -> None
-    | Some s -> s
-  in
+  let root_path = Option.join ip.rootPath in
   { workspace_folders; root_uri; root_path }
 ;;
 
 let on_change t { DidChangeWorkspaceFoldersParams.event = { added; removed } } =
   let workspace_folders =
-    let init = Option.value t.workspace_folders ~default:Uri_map.empty in
+    let init = Option.value t.workspace_folders ~default:(Map.empty (module Uri)) in
     let init =
       List.fold_left removed ~init ~f:(fun acc (a : WorkspaceFolder.t) ->
-        Uri_map.remove a.uri acc)
+        Map.remove acc a.uri)
     in
     List.fold_left added ~init ~f:(fun acc (a : WorkspaceFolder.t) ->
-      Uri_map.add acc ~key:a.uri ~data:a)
+      Map.set acc ~key:a.uri ~data:a)
     |> Option.some
   in
   { t with workspace_folders }
@@ -41,24 +36,22 @@ let on_change t { DidChangeWorkspaceFoldersParams.event = { added; removed } } =
 
 let workspace_folders { root_uri; root_path; workspace_folders } =
   match workspace_folders with
-  | Some s -> Uri_map.bindings s |> List.map ~f:snd
+  | Some workspace_folders -> Map.data workspace_folders
   | None ->
     (* WorkspaceFolders has the most priority. Then rootUri and finally
        rootPath *)
-    (match workspace_folders, root_uri, root_path with
-     | Some workspace_folders, _, _ ->
-       Uri_map.bindings workspace_folders |> List.map ~f:snd
-     | _, Some root_uri, _ ->
+    (match root_uri, root_path with
+     | Some root_uri, _ ->
        [ WorkspaceFolder.create
            ~uri:root_uri
            ~name:(Filename.basename (Uri.to_path root_uri))
        ]
-     | _, _, Some root_path ->
+     | None, Some root_path ->
        [ WorkspaceFolder.create
            ~uri:(Uri.of_path root_path)
            ~name:(Filename.basename root_path)
        ]
-     | _ ->
+     | None, None ->
        let cwd = Sys.getcwd () in
        [ WorkspaceFolder.create ~uri:(Uri.of_path cwd) ~name:(Filename.basename cwd) ])
 ;;
