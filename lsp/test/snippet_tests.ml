fbe65baac48085209fb2c38a53f24a028cefba1e
Rudi Grinberg <me@rgrinberg.com>
2026-07-27 15:00:36 +0100

    test(lsp): demonstrate invalid snippet choice escapes
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lsp/test/snippet_tests.ml
+++ lsp/test/snippet_tests.ml
@@ -2,3 +2,10 @@ let%expect_test "snippet text is not escaped" =
   Lsp.Snippet.text "$}\\" |> Lsp.Snippet.to_string |> print_endline;
   [%expect {| $}\ |}]
 ;;
+
+let%expect_test "snippet choices double escape metacharacters" =
+  Lsp.Snippet.choice [ "$"; "}"; "\\"; ","; "|" ]
+  |> Lsp.Snippet.to_string
+  |> print_endline;
+  [%expect {snippet| ${1|\\$,\\},\\,\,,\||} |snippet}]
+;;
