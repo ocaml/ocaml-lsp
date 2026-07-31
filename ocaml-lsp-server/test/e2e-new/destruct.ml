855d48853b684ea6cf66c794606b2e30efb744b2
Rudi Grinberg <me@rgrinberg.com>
2026-07-31 20:31:28 +0100

    test(destruct): expose Not_allowed internal error
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/destruct.ml
+++ ocaml-lsp-server/test/e2e-new/destruct.ml
@@ -26,6 +26,35 @@ module Util = struct
   ;;
 end
 
+let%expect_test "destruct at a non-destructible range raises an internal error" =
+  let source = "let x = 1" in
+  let request client =
+    let position = Position.create ~line:0 ~character:0 in
+    let range = Range.create ~start:position ~end_:position in
+    let* result = Fiber.collect_errors (fun () -> Util.call_destruct client range) in
+    match result with
+    | Error
+        [ { Exn_with_backtrace.exn = Jsonrpc.Response.Error.E error; backtrace = _ } ] ->
+      let data = Option.value_exn error.data in
+      let exn = Yojson.Safe.Util.(data |> member "exn" |> to_string) in
+      Printf.printf
+        "code: %s\nexception: %s\n"
+        (Jsonrpc.Response.Error.Code.to_string error.code)
+        exn;
+      Fiber.return ()
+    | Error errors -> Fiber.reraise_all errors
+    | Ok response ->
+      Test.print_result response;
+      Fiber.return ()
+  in
+  Helpers.test source request;
+  [%expect
+    {|
+    code: InternalError
+    exception: Merlin_analysis.Destruct.Not_allowed("value_binding")
+    |}]
+;;
+
 let%expect_test "Perform `destruct` as custom request - 1" =
   let source =
     {|
