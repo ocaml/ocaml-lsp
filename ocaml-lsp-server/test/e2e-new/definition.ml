587723153a8a6befc92cb5bcbf3294a396ebaee5
Rudi Grinberg <me@rgrinberg.com>
2026-07-31 10:34:33 +0100

    test(definition): expose Not_found for nullary exception
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/definition.ml
+++ ocaml-lsp-server/test/e2e-new/definition.ml
@@ -2,6 +2,17 @@ open Test.Import
 
 let print_locations = Test.print_option Locations.yojson_of_t
 
+let rec censor_backtraces = function
+  | `Assoc fields ->
+    `Assoc
+      (List.map fields ~f:(fun (name, value) ->
+         if String.equal name "backtrace"
+         then name, `String "<censored>"
+         else name, censor_backtraces value))
+  | `List values -> `List (List.map values ~f:censor_backtraces)
+  | json -> json
+;;
+
 let definition client position =
   let textDocument = TextDocumentIdentifier.create ~uri:Helpers.uri in
   Client.request
@@ -50,6 +61,32 @@ let%expect_test "reports definition lookup failures without stopping the server"
     missing: Request "Jump to definition" failed. ("Locate: Not in environment: missing")
     builtin: Request "Jump to definition" failed. ("Locate: \"int\" is a builtin, it is not possible to jump to its definition")
     server remains usable: true
+    |}]
+;;
+
+let%expect_test "definition of a nullary exception leaks Not_found" =
+  let source = "exception E\nE" in
+  Helpers.test source (fun client ->
+    let* result =
+      Fiber.collect_errors (fun () ->
+        definition client (Position.create ~line:1 ~character:1))
+    in
+    match result with
+    | Error [ { Exn_with_backtrace.exn = Jsonrpc.Response.Error.E error; backtrace = _ } ]
+      ->
+      Jsonrpc.Response.Error.yojson_of_t error |> censor_backtraces |> Test.print_result;
+      Fiber.return ()
+    | Error errors -> Fiber.reraise_all errors
+    | Ok response ->
+      print_locations response;
+      Fiber.return ());
+  [%expect
+    {|
+    {
+      "data": { "exn": "Not_found", "backtrace": "<censored>" },
+      "code": -32603,
+      "message": "uncaught exception"
+    }
     |}]
 ;;
 
