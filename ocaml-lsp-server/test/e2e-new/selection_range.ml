8c5cf75e82b7fa8d328eb8585a3b91de35df82be
Rudi Grinberg <me@rgrinberg.com>
2026-09-04 08:11:50 +0100

    test(selection-range): expose non-enclosing recovery range (#2168)
    Record that Merlin recovery can return a selection range that does not contain the requested position.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- ocaml-lsp-server/test/e2e-new/selection_range.ml
+++ ocaml-lsp-server/test/e2e-new/selection_range.ml
@@ -232,6 +232,21 @@ let%expect_test "comment-only document without a trailing newline" =
     |}]
 ;;
 
+let%expect_test "selection range does not contain the requested position during recovery" =
+  test "\nx" [ Position.create ~line:1 ~character:1 ];
+  [%expect
+    {|
+    [
+      [
+        {
+          "end": { "character": 0, "line": 1 },
+          "start": { "character": 0, "line": 0 }
+        }
+      ]
+    ]
+    |}]
+;;
+
 let%expect_test "returns a reasonable selection range in the presence of syntax errors" =
   let source =
     {ocaml|module M = struct
